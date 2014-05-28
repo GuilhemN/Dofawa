d8ec0b51b472c8aa4168b67e39defc42dcaefe58
Ener-Getick <ti.guilou@hotmail.fr>
2014-05-28 19:19:15 +0200

    fixup! Create the profile controller

--- src/Dof/UserBundle/Controller/ProfileController.php
+++ src/Dof/UserBundle/Controller/ProfileController.php
@@ -6,7 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class ProfileController extends Controller
 {
 	/**
 	 * @Route("/{name_user}")
