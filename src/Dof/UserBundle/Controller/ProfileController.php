91c8d93521607c4ba0112defe2e686253a6ce55f
Ener-Getick <ti.guilou@hotmail.fr>
2014-05-28 19:13:43 +0200

    Create the profile controller

---
+++ src/Dof/UserBundle/Controller/ProfileController.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Dof\UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+	/**
+	 * @Route("/{name_user}")
+	 * @ParamConverter("user", class="DofUserBundle:User", options={"username" = "name_user"})
+	 */
+    public function indexAction(User $user)
+    {
+        return $this->render('DofUserBundle:Profile:index.html.twig', array('user' => $user));
+    }
+}
