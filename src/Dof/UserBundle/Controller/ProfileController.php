119111fc542d7a920feab296180c4904c7f031d8
Ener-Getick <ti.guilou@hotmail.fr>
2014-06-05 22:37:17 +0200

    Fix a bug

--- src/Dof/UserBundle/Controller/ProfileController.php
+++ src/Dof/UserBundle/Controller/ProfileController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 use Dof\UserBundle\Entity\User;
 
-use FOS\UserBundle\Controller as BaseController;
+use FOS\UserBundle\Controller\ProfileController as BaseController;
 
 class ProfileController extends BaseController
 {
