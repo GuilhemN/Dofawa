eeaf55b1b7c770de6725347abc5db6a9fdc46d0f
Ener-Getick <ti.guilou@hotmail.fr>
2014-06-16 21:56:54 +0200

    Change the profile config

--- src/Dof/UserBundle/Controller/ProfileController.php
+++ src/Dof/UserBundle/Controller/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends BaseController
 	/**
 	 * @ParamConverter("user", options={"mapping": {"name_user": "slug"}})
 	 */
-    public function indexAction(User $user)
+    public function userpageAction(User $user)
     {
         return $this->container->get('templating')->renderResponse('DofUserBundle:Profile:index.html.twig', array('user' => $user));
     }
