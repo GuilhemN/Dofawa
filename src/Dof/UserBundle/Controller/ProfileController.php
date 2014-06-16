21e1e566a91c62794b3ded7f15fb0c2884c2d076
Ener-Getick <ti.guilou@hotmail.fr>
2014-06-16 22:10:38 +0200

    Change the profile template

--- src/Dof/UserBundle/Controller/ProfileController.php
+++ src/Dof/UserBundle/Controller/ProfileController.php
@@ -21,4 +21,17 @@ class ProfileController extends BaseController
     {
         return $this->container->get('templating')->renderResponse('DofUserBundle:Profile:index.html.twig', array('user' => $user));
     }
+
+    /**
+     * Show the user
+     */
+    public function showAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->container->get('templating')->renderResponse('DofUserBundle:Profile:index.html.twig', array('user' => $user));
+    }
 }
