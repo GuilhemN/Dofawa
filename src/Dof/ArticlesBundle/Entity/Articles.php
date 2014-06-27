4ab7bf33d9ddceb92a447bc19f34902b2b90d601
Ener-Getick <energetick.guigui@gmail.com>
2014-06-27 13:54:11 +0200

    Change the articles entity

--- src/Dof/ArticlesBundle/Entity/Articles.php
+++ src/Dof/ArticlesBundle/Entity/Articles.php
@@ -12,6 +12,10 @@ use XN\DataBundle\SluggableTrait;
 use XN\DataBundle\OwnableInterface;
 use Dof\UserBundle\OwnableTrait;
 
+//Traduction Titre/Description
+use XN\DataBundle\LocalizedNameTrait;
+use XN\DataBundle\LocalizedDescriptionTrait;
+
 /**
  * Articles
  *
@@ -29,63 +33,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
      */
     private $id;
 
-    use TimestampableTrait, SluggableTrait, OwnableTrait;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="titre", type="string", length=150)
-     */
-    private $titre;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="message", type="text", nullable=true)
-     */
-    private $message;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="tren", type="string", length=150)
-     */
-    private $trEn;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="tres", type="string", length=150)
-     */
-    private $trEs;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="trde", type="string", length=150)
-     */
-    private $trDe;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="txen", type="text", nullable=true)
-     */
-    private $txEn;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="txes", type="text", nullable=true)
-     */
-    private $txEs;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="txde", type="text", nullable=true)
-     */
-    private $txDe;
+    use TimestampableTrait, SluggableTrait, OwnableTrait, LocalizedNameTrait, LocalizedDescriptionTrait;
 
     /**
      * @var string
@@ -126,195 +74,11 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set titre
-     *
-     * @param string $titre
-     * @return Articles
-     */
-    public function setTitre($titre)
-    {
-        $this->titre = $titre;
-
-        return $this;
-    }
-
-    /**
-     * Get titre
-     *
-     * @return string 
-     */
-    public function getTitre()
-    {
-        return $this->titre;
-    }
-
-    /**
-     * Set message
-     *
-     * @param string $message
-     * @return Articles
-     */
-    public function setMessage($message)
-    {
-        $this->message = $message;
-
-        return $this;
-    }
-
-    /**
-     * Get message
-     *
-     * @return string 
-     */
-    public function getMessage()
-    {
-        return $this->message;
-    }
-
-    /**
-     * Set trEn
-     *
-     * @param string $trEn
-     * @return Articles
-     */
-    public function setTrEn($trEn)
-    {
-        $this->trEn = $trEn;
-
-        return $this;
-    }
-
-    /**
-     * Get trEn
-     *
-     * @return string 
-     */
-    public function getTrEn()
-    {
-        return $this->trEn;
-    }
-
-    /**
-     * Set trEs
-     *
-     * @param string $trEs
-     * @return Articles
-     */
-    public function setTrEs($trEs)
-    {
-        $this->trEs = $trEs;
-
-        return $this;
-    }
-
-    /**
-     * Get trEs
-     *
-     * @return string 
-     */
-    public function getTrEs()
-    {
-        return $this->trEs;
-    }
-
-    /**
-     * Set trDe
-     *
-     * @param string $trDe
-     * @return Articles
-     */
-    public function setTrDe($trDe)
-    {
-        $this->trDe = $trDe;
-
-        return $this;
-    }
-
-    /**
-     * Get trDe
-     *
-     * @return string 
-     */
-    public function getTrDe()
-    {
-        return $this->trDe;
-    }
-
-    /**
-     * Set txEn
-     *
-     * @param string $txEn
-     * @return Articles
-     */
-    public function setTxEn($txEn)
-    {
-        $this->txEn = $txEn;
-
-        return $this;
-    }
-
-    /**
-     * Get txEn
-     *
-     * @return string 
-     */
-    public function getTxEn()
-    {
-        return $this->txEn;
-    }
-
-    /**
-     * Set txEs
-     *
-     * @param string $txEs
-     * @return Articles
-     */
-    public function setTxEs($txEs)
-    {
-        $this->txEs = $txEs;
-
-        return $this;
-    }
-
-    /**
-     * Get txEs
-     *
-     * @return string 
-     */
-    public function getTxEs()
-    {
-        return $this->txEs;
-    }
-
-    /**
-     * Set txDe
-     *
-     * @param string $txDe
-     * @return Articles
-     */
-    public function setTxDe($txDe)
-    {
-        $this->txDe = $txDe;
-
-        return $this;
-    }
-
-    /**
-     * Get txDe
-     *
-     * @return string 
-     */
-    public function getTxDe()
-    {
-        return $this->txDe;
     }
 
     /**
@@ -333,7 +97,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get cles
      *
-     * @return string 
+     * @return string
      */
     public function getCles()
     {
@@ -356,7 +120,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get validation
      *
-     * @return integer 
+     * @return integer
      */
     public function getValidation()
     {
@@ -379,7 +143,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get mail
      *
-     * @return string 
+     * @return string
      */
     public function getMail()
     {
@@ -402,7 +166,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get categorie
      *
-     * @return string 
+     * @return string
      */
     public function getCategorie()
     {
@@ -425,7 +189,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get news
      *
-     * @return integer 
+     * @return integer
      */
     public function getNews()
     {
@@ -448,7 +212,7 @@ class Articles implements IdentifiableInterface, TimestampableInterface, Sluggab
     /**
      * Get journal
      *
-     * @return string 
+     * @return string
      */
     public function getJournal()
     {
