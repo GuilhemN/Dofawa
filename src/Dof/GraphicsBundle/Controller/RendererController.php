53cb19a43a279c469761dc8c26e2fdc9ade6c11f
Nicolas L. / Exter-N <exter-n@exter-n.fr>
2014-07-04 23:18:06 +0200

    Proxy to the renderer (TEMPORARY !!!!!)

---
+++ src/Dof/GraphicsBundle/Controller/RendererController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dof\GraphicsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class RendererController extends Controller
+{
+    public function lookAction($look, $focus, $direction, $width, $height, $padding)
+    {
+        // FIXME : This proxy is only TEMPORARY !!!!!
+        $response = new Response(file_get_contents('http://staticns.ankama.com/dofus/renderer/look/' . $look . '/' . $focus . '/' . $direction . '/' . $width . '_' . $height . '-' . $padding . '.png'));
+        $response->headers->set('Content-Type', 'image/png');
+        return $response;
+    }
+}
