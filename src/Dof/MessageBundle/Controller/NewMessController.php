525361c0ecaf20ce64a55aa2448eaa6d443bcc32
menthosaire <sebastien.michon17@gmail;com>
2014-07-09 23:41:43 +0200

    Add controller for new messages

---
+++ src/Dof/MessageBundle/Controller/NewMessController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Dof\MessageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class NewMessController extends Controller
+{
+	public function indexAction()
+	{
+		$session = $this->get('session');
+
+		$timestamp = $session->get('message/timestamp');
+		$hasNew = $session->get('message/hasNew');
+		$countNews = $session->get('message/countNews');
+		
+		$idUser = $session->get('id');
+		$now = time();
+		
+		// MàJ
+		if (($now - $timestamp) > 60 && !$hasNew){
+			$repository = $this->getDoctrine()->getRepository('DofMessageBundle:MessageMetadata');
+			$countNews = $repository->findBy(array('participant_id' => $idUser), array('is_read' => 0))->count();
+
+			if ($countNews <=0)
+				$countNews = 0;
+
+			// Sauvegarde en session
+			$session->set('message/hasNew',true);
+			$session->set('message/timestamp',time());
+			$session->set('message/countNews',$countNews);
+		}
+		
+		$response = '<span class="glyphicon glyphicon-envelope"></span> Messages <span class="badge">'.$countNews.'</span>';
+		return new Response($response);
+	}
+}
