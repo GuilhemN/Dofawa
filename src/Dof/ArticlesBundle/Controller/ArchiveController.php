0c571ddf58fef81ebf6a7fa4084963c89f47407f
menthosaire <sebastien.michon17@gmail;com>
2014-07-11 00:02:01 +0200

    fix bug function

--- src/Dof/ArticlesBundle/Controller/ArchiveController.php
+++ src/Dof/ArticlesBundle/Controller/ArchiveController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArchiveController extends Controller
 {
-	public function indexAction($type = '')
+	public function archiveAction()
 	{
 		$u = $this->get('security.context')->getToken()->getUser();
 		$translator = $this->get('translator');
