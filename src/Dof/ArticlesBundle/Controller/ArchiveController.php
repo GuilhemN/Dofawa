e9b2f35df0bbe7b113c848a3eb11613bf195efec
menthosaire <sebastien.michon17@gmail;com>
2014-07-10 23:36:22 +0200

    Add archive page

---
+++ src/Dof/ArticlesBundle/Controller/ArchiveController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dof\ArticlesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArchiveController extends Controller
+{
+	public function indexAction($type = '')
+	{
+		$u = $this->get('security.context')->getToken()->getUser();
+		$translator = $this->get('translator');
+
+		$em = $this->getDoctrine()->getManager();
+		$articles = $em->getRepository('DofArticlesBundle:Article')->findArticlesWithLimits(true);
+
+		foreach ($articles as $k => &$article) {
+			$content = $article->getDescription($translator->getLocales());
+			$article->setDescription(preg_replace('/<img(.*?)>/', '', $content), $translator->getLocales());
+		}
+
+		return $this->render('DofArticlesBundle:Archive:archive.html.twig', array('articles'=>$articles));
+
+	}
+}
