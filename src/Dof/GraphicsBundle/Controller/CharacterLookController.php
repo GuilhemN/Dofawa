ec9cec65402c08583ec5d50de075517f743812be
Ener-Getick <energetick.guigui@gmail.com>
2014-07-11 17:09:36 +0200

    Add an handler

--- src/Dof/GraphicsBundle/Controller/CharacterLookController.php
+++ src/Dof/GraphicsBundle/Controller/CharacterLookController.php
@@ -33,16 +33,49 @@ class CharacterLookController extends Controller
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            if(!$this->get('security.context')->isGranted('ROLE_STYLIST'))
-                $cl->setPubliclyVisible(0);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($cl);
-            $em->flush();
+            $this->handler($form, $cl)
         }
 
         return $this->render('DofGraphicsBundle:CharacterLook:create.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    protected function handler($form, $cl){
+        if(!$this->get('security.context')->isGranted('ROLE_STYLIST'))
+            $cl->setPubliclyVisible(0);
+
+        $data = $form->getData();
+
+        $skinned = $this->getDoctrine()->getManager()
+                        ->getRepository('DofItemsBundle:SkinnedEquipmentTemplate');
+        $animal  = $this->getDoctrine()->getManager()
+                        ->getRepository('DofItemsBundle:AnimalTemplate');
+        $weapon  = $this->getDoctrine()->getManager()
+                        ->getRepository('DofItemsBundle:WeaponTemplate');
+
+        // Vérif et liage cape, coiffe et bouclier
+        $skinnedItems = ['shield' => 7, 'hat' => 10, 'cloak' => 11];
+        foreach($skinnedItems as $item => $slot){
+            $id = (int) $data[$item];
+            $item = $skinned->findById($id);
+
+            if(!empty($item) && $item->getSlot() == $slot && !empty($item->getSkin()))
+                $lg->{'set'.ucfirst($item)}($item);
+        }
+
+        // Liage Arme
+        $item = $weapon->findById($data['weapon']);
+        if(!empty($item) && !empty($item->getSkin()))
+            $lg->setWeapon($item);
+
+        // Liage Familier
+        $item = $animal->findById($data['animal']);
+        if(!empty($item) && !empty($item->getBone()))
+            $lg->setAnimal($item);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($cl);
+        $em->flush();
     }
 }
