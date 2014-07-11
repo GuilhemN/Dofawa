a36b95d3221448f65ea5283efa443d1b8a53b543
menthosaire <sebastien.michon17@gmail;com>
2014-07-11 21:26:26 +0200

    Add pagination in archive

--- src/Dof/ArticlesBundle/Controller/ArchiveController.php
+++ src/Dof/ArticlesBundle/Controller/ArchiveController.php
@@ -6,20 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArchiveController extends Controller
 {
-	public function archiveAction()
+	public function archiveAction($page)
 	{
 		$u = $this->get('security.context')->getToken()->getUser();
 		$translator = $this->get('translator');
 
+		// 15 results per page
+		$lastresult = 15 * $page;
+		$firstresult = $lastresult - 14;
+
 		$em = $this->getDoctrine()->getManager();
-		$articles = $em->getRepository('DofArticlesBundle:Article')->findArticlesWithLimits(true);
+		$articles = $em->getRepository('DofArticlesBundle:Article')->setFirstResult($firstresult)->setMaxResults($lastresult);
 
 		foreach ($articles as $k => &$article) {
 			$content = $article->getDescription($translator->getLocales());
 			$article->setDescription(preg_replace('/<img(.*?)>/', '', $content), $translator->getLocales());
 		}
 
-		return $this->render('DofArticlesBundle:Archive:archive.html.twig', array('articles'=>$articles));
+		return $this->render('DofArticlesBundle:Archive:archive.html.twig', array('articles'=>$articles, 'page'=>$page));
 
 	}
 }
