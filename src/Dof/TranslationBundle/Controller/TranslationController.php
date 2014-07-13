0130f83a361fb1410e547779fbe8f39b12e82eac
Ener-Getick <energetick.guigui@gmail.com>
2014-07-14 01:15:17 +0200

    Add a return

--- src/Dof/TranslationBundle/Controller/TranslationController.php
+++ src/Dof/TranslationBundle/Controller/TranslationController.php
@@ -39,7 +39,7 @@ class TranslationController extends Controller
 
         $form = $this->createForm('dof_translation', $translation);
 
-        $this->render('DofTranslationBundle:Translation:create.html.twig', ['form' => $form->createView()]);
+        return $this->render('DofTranslationBundle:Translation:create.html.twig', ['form' => $form->createView()]);
     }
 
     private function domainExclude(){
