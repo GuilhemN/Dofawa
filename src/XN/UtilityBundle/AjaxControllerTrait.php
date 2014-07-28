6efd4adb15e50def6aa4f869d9ac69b5a20a61ae
Ener-Getick <energetick.guigui@gmail.com>
2014-07-28 19:13:50 +0200

    Add ajax support

--- src/XN/UtilityBundle/AjaxControllerTrait.php
+++ src/XN/UtilityBundle/AjaxControllerTrait.php
@@ -6,10 +6,18 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 trait AjaxControllerTrait
 {
+	public function createAdaptedResponse(array $params, $status = 200, $headers = array()){
+		if($this->get('request')->isXmlHttpRequest())
+			return $this->createJsonResponse($params, $status, $headers);
+		else
+			return $this->render('::layout.html.twig', $params);
+	}
+
 	public function createJsonResponse($data = null, $status = 200, $headers = array())
 	{
 		return new JsonResponse($data, $status, $headers);
 	}
+
 	public function createJsonPResponse($data = null, $callback = null, $status = 200, $headers = array())
 	{
 		$response = new JsonResponse($data, $status, $headers);
