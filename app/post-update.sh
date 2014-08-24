6ead9c0a203f1a379037235314aaac9c7623079d
Exter-N <exter-n@exter-n.fr>
2014-08-25 00:32:29 +0200

    Reverted to standard PHP for post-update script

--- app/post-update.sh
+++ app/post-update.sh
@@ -2,6 +2,16 @@
 
 cd "$(dirname "$(dirname "$0")")"
 
+if type php5 >/dev/null 2>&1; then
+	function php {
+		php5 "$@"
+	}
+else
+	function php {
+		php "$@"
+	}
+fi
+
 mkdir -p app/logs
 mkdir -p bin/cache
 
@@ -9,20 +19,20 @@ rm -rf app/cache
 mkdir -p app/cache
 
 if [ "$1" == --dev ]; then
-	composer install --no-scripts
+	php "$(type -p composer)" install --no-scripts
 else
-	composer install -o --no-scripts
+	php "$(type -p composer)" install -o --no-scripts
 fi
 cp -f pinned/PDOConnection.php vendor/doctrine/dbal/lib/Doctrine/DBAL/Driver
 cp -f pinned/PDOStatement.php vendor/doctrine/dbal/lib/Doctrine/DBAL/Driver
 cp -f pinned/QueryBuilder.php vendor/doctrine/orm/lib/Doctrine/ORM
 
-app/console cache:clear -e dev &
-app/console cache:clear -e prod
+php app/console cache:clear -e dev &
+php app/console cache:clear -e prod
 wait "$!"
 
-app/console doctrine:schema:update --dump-sql
-app/console doctrine:schema:update --force
+php app/console doctrine:schema:update --dump-sql
+php app/console doctrine:schema:update --force
 
 if [ "$(id -u)" == 0 ]; then
 	chown -R www-data:www-data app/cache
@@ -31,7 +41,7 @@ if [ "$(id -u)" == 0 ]; then
 fi
 
 if [ "$1" == --dev ]; then
-	app/console assets:install --symlink --relative
+	php app/console assets:install --symlink --relative
 else
-	app/console assets:install
+	php app/console assets:install
 fi
