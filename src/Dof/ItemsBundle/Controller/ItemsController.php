d12eb9301c54101a101a921a209585b5041915b5
Ener-Getick <energetick.guigui@gmail.com>
2014-08-24 23:26:02 +0200

    Add a test design

---
+++ src/Dof/ItemsBundle/Controller/ItemsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dof\ItemsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ItemsController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('DofItemsBundle:EquipmentTemplate');
+
+        $item = $repo->findOneBy(array());
+        return $this->render('DofItemsBundle:Items:index.html.twig', ['item' => $item]));
+    }
+}
