57aebdc419c8da9774b6a6fa48e8d9fe59ea9a99
Ener-Getick <energetick.guigui@gmail.com>
2014-08-28 15:13:34 +0200

    Fix a bug

--- src/Dof/ItemsBundle/Controller/DefaultController.php
+++ src/Dof/ItemsBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('DofItemsBundle:Default:index.html.twig', array('name' => ''));
+        return $this->render('DofItemsBundle:Default:index.html.twig'));
     }
 }
