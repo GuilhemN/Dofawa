1cd68b0d1acc0daf21ac4f96f29cc7cff9efde14
menthosaire <sebastien.michon17@gmail;com>
2014-08-29 15:37:30 +0200

    Add bundle to create forum

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -50,6 +50,7 @@ class AppKernel extends Kernel
             new Dof\MessageBundle\DofMessageBundle(),
             new Dof\TranslationBundle\DofTranslationBundle(),
             new Dof\BuildBundle\DofBuildBundle(),
+            new Dof\ForumBundle\DofForumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
