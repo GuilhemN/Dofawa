5239dca644402e6fba129b737bafa52e03ba77bf
Ener-Getick <energetick.guigui@gmail.com>
2014-09-05 17:32:33 +0200

    Fix a bug

--- src/Dof/MainBundle/NotificationManager.php
+++ src/Dof/MainBundle/NotificationManager.php
@@ -93,7 +93,7 @@ class NotificationManager
                     foreach($fields as $field)
                         if($field == 'localeName')
                             $value = $value->getName($this->di->get('translator')->getLocales());
-                        elseif(strtolower($field) == 'currentuser')
+                        elseif($field == 'currentuser')
                             $value = $this->di->get('security.context')->getToken()->getUser();
                         else
                             $value = $value->{'get' . ucfirst($field)}();
@@ -113,7 +113,12 @@ class NotificationManager
 
                     $value = $ent;
                     foreach($fields as $field)
-                        $value = $ent->{'get' . ucfirst($field)}();
+                        if($field == 'localeName')
+                            $value = $value->getName($this->di->get('translator')->getLocales());
+                        elseif($field == 'currentuser')
+                            $value = $this->di->get('security.context')->getToken()->getUser();
+                        else
+                            $value = $ent->{'get' . ucfirst($field)}();
 
                     $pathParams[$k] = $value;
                 }
