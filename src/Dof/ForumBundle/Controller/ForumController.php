91b262a048448265118c5a33be35edc5d12973ad
menthosaire <sebastien.michon17@gmail;com>
2014-09-12 20:27:00 +0200

    Edit controller for add topic

--- src/Dof/ForumBundle/Controller/ForumController.php
+++ src/Dof/ForumBundle/Controller/ForumController.php
@@ -76,8 +76,8 @@ class ForumController extends Controller
     	$topic =new Topic;
     	$message =new Message;
 
+    	$topic->addMessage($message);
     	
-
     	$formtopic = $this->createForm(new TopicType, $topic);
 
 		$request = $this->get('request');
@@ -88,8 +88,8 @@ class ForumController extends Controller
 
 		    	$topic->setForum($forum);
 		    	$topic->setLocked(0);
-				$topic->addMessage($message);
-				
+		    	$message->setTopic($topic);
+
 		    	$em = $this->getDoctrine()->getManager();
 		      	$em->persist($topic);
 		      	$em->persist($message);
