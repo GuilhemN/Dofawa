d3e23886cc77963371b9d89e9e57a449277fefa3
Ener-Getick <energetick.guigui@gmail.com>
2014-09-13 18:15:02 +0200

    Add the sets on the admin

---
+++ src/Dof/ItemsBundle/Admin/ItemSetAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Dof\ItemsBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ItemSetAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('nameFr')
+            ->add('level')
+            ->add('itemCount')
+            ->add('slug')
+            ->add('release')
+            ->add('preliminary')
+            ->add('deprecated')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('nameFr')
+            ->add('level')
+            ->add('itemCount')
+            ->add('release')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('level')
+            ->add('itemCount')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('slug')
+            ->add('release')
+            ->add('preliminary')
+            ->add('deprecated')
+            ->add('nameFr')
+            ->add('nameEn')
+            ->add('nameDe')
+            ->add('nameEs')
+            ->add('nameIt')
+            ->add('namePt')
+            ->add('nameJa')
+            ->add('nameRu')
+            ->add('primaryBonus')
+        ;
+    }
+}
