74b059cda80a822a356eb3ff9e2a5d1cde5b29f6
Ener-Getick <energetick.guigui@gmail.com>
2014-09-13 19:51:30 +0200

    Fix a bug

--- src/Dof/MainBundle/Doctrine/SetUpdater.php
+++ src/Dof/MainBundle/Doctrine/SetUpdater.php
@@ -10,6 +10,16 @@ use Dof\ItemsBundle\Entity\ItemSet;
 
 class SetUpdater
 {
+	public function prePersist(LifecycleEventArgs $args)
+	{
+		$ent = $args->getEntity();
+		if ($ent instanceof ItemSet) {
+			$em = $args->getEntityManager();
+
+			$id = $em->getRepository('DofItemsBundle:ItemSet')->getMinimalId() - 1;
+			$ent->setId($id);
+		}
+	}
 	public function preRemove(LifecycleEventArgs $args)
 	{
 		$ent = $args->getEntity();
@@ -38,44 +48,30 @@ class SetUpdater
 		$uow = $em->getUnitOfWork();
 		$mds = array();
 		$updates = array_filter($uow->getScheduledEntityUpdates(), function ($ent) use ($uow) {
-			return $ent instanceof ItemSet or $ent instanceof EquipmentTemplate && self::hasCharactsChanges($ent, $uow->getEntityChangeSet($ent));
+			return $ent instanceof EquipmentTemplate && self::hasCharactsChanges($ent, $uow->getEntityChangeSet($ent));
 		});
 		foreach ($updates as $ent) {
-			if($ent instanceof ItemSet && $ent->getId() == ''){
-				$id = $em->getRepository('DofItemsBundle:ItemSet')->getMinimalId() - 1;
-				$ent->setId($id);
+			foreach($ent->getOriginalSets() as $set){
+				$maxLevel = 0;
+				foreach($set->getItems() as $item){
+					if($maxLevel < $item->getLevel())
+						$maxLevel = $item->getLevel();
+					if($item->getLevel() == 200)
+						break;
+				}
 
-				$clazz = get_class($ent);
+				$set->setLevel($maxLevel);
+				$set->setItemCount(count($set->getItems()));
+
+				$clazz = get_class($set);
 				if (isset($mds[$clazz]))
 					$md = $mds[$clazz];
 				else {
 					$md = $em->getClassMetadata($clazz);
 					$mds[$clazz] = $md;
 				}
-				$uow->recomputeSingleEntityChangeSet($md, $ent);
+				$uow->recomputeSingleEntityChangeSet($md, $set);
 			}
-			elseif($ent instanceof EquipmentTemplate)
-				foreach($ent->getOriginalSets() as $set){
-					$maxLevel = 0;
-					foreach($set->getItems() as $item){
-						if($maxLevel < $item->getLevel())
-							$maxLevel = $item->getLevel();
-						if($item->getLevel() == 200)
-							break;
-					}
-
-					$set->setLevel($maxLevel);
-					$set->setItemCount(count($set->getItems()));
-
-					$clazz = get_class($set);
-					if (isset($mds[$clazz]))
-						$md = $mds[$clazz];
-					else {
-						$md = $em->getClassMetadata($clazz);
-						$mds[$clazz] = $md;
-					}
-					$uow->recomputeSingleEntityChangeSet($md, $set);
-				}
 		}
 	}
 
