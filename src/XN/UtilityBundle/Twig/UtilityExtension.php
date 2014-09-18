93ccaa545695719f41c34b1105726eebeee4edde
Ener-Getick <energetick.guigui@gmail.com>
2014-09-18 19:18:20 +0200

    Add a "just now" for the date formater

--- src/XN/UtilityBundle/Twig/UtilityExtension.php
+++ src/XN/UtilityBundle/Twig/UtilityExtension.php
@@ -115,6 +115,8 @@ class UtilityExtension extends \Twig_Extension
 			if ($type == 'medium' && ($diff->y && $diff->m) == 0 )
 				if($diff->d == 1 or (date('G') < $diff->h && $diff->d == 0))
 					$format = $this->dateParams('formats.1.yesterdayAt', $locale);
+				elseif($diff->d == 0 && $diff->h = 0 and $diff->m = 0)
+					return $this->translator->trans('formats.1.justNow', [], $locale);
 				elseif($diff->d == 0 && $diff->h <= 2)
 					return $this->translator->trans('formats.1.xHoursAgo', ['%h%' => $diff->h, '%m%' => $diff->m], 'date', $locale);
 				elseif($diff->d == 0)
