56bae7dd77d398e3e80b3e9522df87c048d291be
Ener-Getick <energetick.guigui@gmail.com>
2014-09-19 21:34:47 +0200

    Fix a bug

--- src/Dof/MainBundle/Resources/public/js/notifications.js
+++ src/Dof/MainBundle/Resources/public/js/notifications.js
@@ -1,7 +1,7 @@
 jQuery(function () {
     var unread = jQuery('#notifications span.badge').html();
     if(unread > 0)
-        document.title = '(' + unread + ') ' + document.title;
+        majNotificationsTitle(unread);
 
     jQuery('#notifications').on('show.bs.dropdown', function (){
         jQuery.ajax({
@@ -9,6 +9,8 @@ jQuery(function () {
         }).done(function(data) {
             jQuery('#notifications .dropdown-menu').html(data.html);
             jQuery('#notifications span.badge').html(data.unread);
+
+            majNotificationsTitle(data.unread);
         });
     });
 });
@@ -18,13 +20,15 @@ function checkUnreadNotifications(){
         url: Routing.generate('dof_notifications_ajax_check_unread')
     }).done(function(data) {
         jQuery('#notifications span.badge').html(data.unread);
-
-        if(document.title.match(/^\([0-9]+\)/))
-            document.replace(/^\([0-9]+\)/, '(' + data.unread + ')');
-        else
-            document.title = '(' + data.unread + ') ' + document.title;
-
+        majNotificationsTitle(data.unread);
     });
+}
+
+function majNotificationsTitle(unread){
+    if(document.title.match(/^\([0-9]+\)/))
+        document.replace(/^\([0-9]+\)/, '(' + unread + ')');
+    else
+        document.title = '(' + unread + ') ' + document.title;
 }
 
 setInterval(checkUnreadNotifications, 25000)
