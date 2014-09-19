693ec8ff9767fb8cc0f9da2aa7c3be249c9f1e4f
Ener-Getick <energetick.guigui@gmail.com>
2014-09-19 20:48:03 +0200

    test

--- src/Dof/MainBundle/Resources/public/js/notifications.js
+++ src/Dof/MainBundle/Resources/public/js/notifications.js
@@ -3,7 +3,6 @@ jQuery(function () {
             jQuery.ajax({
                 url: Routing.generate('dof_notifications_get_notifications')
             }).done(function(data) {
-                data = JSON.parse(data);
                 jQuery('#notifications .dropdown-menu').html(data.html);
                 jQuery('#notifications span.badge').html(data.unread);
             });
