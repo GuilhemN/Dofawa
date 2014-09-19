c8a3cbae2d534d84b2256304e8f024bff1cd3a0b
Ener-Getick <energetick.guigui@gmail.com>
2014-09-19 20:49:59 +0200

    Fix a "bug"

--- src/Dof/MainBundle/Resources/public/js/notifications.js
+++ src/Dof/MainBundle/Resources/public/js/notifications.js
@@ -1,10 +1,13 @@
 jQuery(function () {
     jQuery('#notifications').on('show.bs.dropdown', function (){
-            jQuery.ajax({
-                url: Routing.generate('dof_notifications_get_notifications')
-            }).done(function(data) {
-                jQuery('#notifications .dropdown-menu').html(data.html);
-                jQuery('#notifications span.badge').html(data.unread);
-            });
+        jQuery.ajax({
+            url: Routing.generate('dof_notifications_get_notifications')
+        }).done(function(data) {
+            jQuery('#notifications .dropdown-menu').html(data.html);
+            jQuery('#notifications span.badge').html(data.unread);
+        });
+    });
+    jQuery('#notifications').on('hidden.bs.dropdown', function () {
+        jQuery('#notifications span.badge').html('0')
     });
 });
