99ca04d6e0c7371f9d52a328848338517a668cb6
menthosaire <sebastien.michon17@gmail;com>
2014-09-20 12:18:49 +0200

    Edit Admin form for forum

--- src/Dof/ForumBundle/Admin/MessageAdmin.php
+++ src/Dof/ForumBundle/Admin/MessageAdmin.php
@@ -34,7 +34,7 @@ class MessageAdmin extends Admin
             ->add('id')
             ->add('owner')
             ->add('updater')
-            ->add('topic')
+            ->add('topic', array('required' => false))
             ->add('content')
             ->add('createdAt')
             ->add('updatedAt')
