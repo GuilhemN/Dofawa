eae2ad62e81b54f9ee106df0f22a724031661d5b
menthosaire <sebastien.michon17@gmail.com>
2014-09-21 23:00:47 +0200

    Fix noob bug

--- src/Dof/ForumBundle/Entity/Topic.php
+++ src/Dof/ForumBundle/Entity/Topic.php
@@ -4,7 +4,6 @@ namespace Dof\ForumBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
-use  Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 
 use XN\Persistence\IdentifiableInterface;
