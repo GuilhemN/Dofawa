15cb06b88d6204a74037541e8ecefe8784212d35
menthosaire <sebastien.michon17@gmail.com>
2014-09-24 19:45:08 +0200

    Forgot )

--- src/Dof/ForumBundle/Entity/Topic.php
+++ src/Dof/ForumBundle/Entity/Topic.php
@@ -306,7 +306,7 @@ class Topic implements IdentifiableInterface, TimestampableInterface, SluggableI
      */
     public function isReadBy(User $user)
     { 
-        foreach ($this->readBy->matching(Criteria::create()->where(Criteria::expr()->eq('id', $user->getId())) as $by){
+        foreach ($this->readBy->matching(Criteria::create()->where(Criteria::expr()->eq('id', $user->getId()))) as $by){
                 if($by == $user)
                     return = true;
         }
