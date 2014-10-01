bc67eefa893e1bbe197e340902d57972f997eff4
Ener-Getick <energetick.guigui@gmail.com>
2014-10-01 21:14:49 +0200

    Change a function name

--- src/Dof/ItemsBundle/Controller/ItemsController.php
+++ src/Dof/ItemsBundle/Controller/ItemsController.php
@@ -37,7 +37,7 @@ class ItemsController extends Controller
     /**
      * @ParamConverter("stuff", class="DofBuildBundle:Stuff", options={"mapping": {"stuff" = "slug"} })
      */
-    public function addItemAction($user, $character, Stuff $stuff, $type, $page){
+    public function showBuildItemsAction($user, $character, Stuff $stuff, $type, $page){
         if($this->getUser()->getSlug() !== $user)
             throw $this->createAccessDeniedException();
 
