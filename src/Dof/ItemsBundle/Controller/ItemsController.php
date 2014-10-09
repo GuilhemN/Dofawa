98680e8d90a9a9c1ea5a6ed1899f6f16368bb04b
Ener-Getick <energetick.guigui@gmail.com>
2014-10-09 20:17:48 +0200

    It's menthosaire !

--- src/Dof/ItemsBundle/Controller/ItemsController.php
+++ src/Dof/ItemsBundle/Controller/ItemsController.php
@@ -34,7 +34,7 @@ class ItemsController extends Controller
         return $this->render('DofItemsBundle:Items:show.html.twig', ['item' => $item]);
     }
 
-    public function showBuildItemsAction(Stuff $stuff, PlayerCharacter $character, User $user, $canWrite, $type){
+    public function showBuildItemsAction(Stuff $stuff, PlayerCharacter $character, User $user, $canWrite, $type, $page){
         if(!$canWrite)
             throw $this->createAccessDeniedException();
 
