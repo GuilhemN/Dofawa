69ff8abc72e3b8fc9f96f1eb0ba3ff57643da6c0
Ener-Getick <energetick.guigui@gmail.com>
2014-10-12 15:41:05 +0200

    test

--- src/XN/Security/TOTPGenerator.php
+++ src/XN/Security/TOTPGenerator.php
@@ -4,11 +4,10 @@ namespace XN\Security;
 use XN\UtilityBundle\TOTPAuthenticationListener;
 
 class TOTPGenerator {
-    public static function genSecret($length = 16){
-        if( $length < 16 || $length % 8 != 0 )
-            $length = 16;
+    public static function genSecret(){
+        $length = 16;
 
-        for(;$length > 0; $length--)
+        while($length--)
             $secret .= array_search(mt_rand(0, 31), TOTPAuthenticationListener::$lut);
 
         return $secret;
