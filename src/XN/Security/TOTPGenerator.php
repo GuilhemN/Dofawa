329c96e5c37717fc28a058a420b6aad424ad4f44
Ener-Getick <energetick.guigui@gmail.com>
2014-10-12 15:23:34 +0200

    test

--- src/XN/Security/TOTPGenerator.php
+++ src/XN/Security/TOTPGenerator.php
@@ -8,7 +8,7 @@ class TOTPGenerator {
         if( $length < 16 || $length % 8 != 0 )
             $length = 16;
 
-        while($length--)
+        while($length--  > 0)    
             $secret .= array_search(mt_rand(0, 31), TOTPAuthenticationListener::$lut);
 
         return $secret;
