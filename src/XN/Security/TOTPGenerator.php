8e20faff2849bbbb13cc2b2f9d86f3a243fac44a
Ener-Getick <energetick.guigui@gmail.com>
2014-10-12 16:32:42 +0200

    fix the qr code

--- src/XN/Security/TOTPGenerator.php
+++ src/XN/Security/TOTPGenerator.php
@@ -4,16 +4,7 @@ namespace XN\Security;
 use XN\UtilityBundle\TOTPAuthenticationListener;
 
 class TOTPGenerator {
-    public static function genSecret($randomString = null){
-        // Decode a random string into binary
-        $seed = $randomString ? $randomString : self::generateRandomString();
-        $secretkey = TOTPAuthenticationListener::base32_decode($seed);
-
-        // Use the algorithm to generate the totp code
-        return $seed;
-    }
-    public function generateRandomString($length = 32)
-    {
+    public static function genSecret($lenght = 16){
         $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
