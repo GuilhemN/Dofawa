259274bc39cdbb48fa19dabb8804afdbd931e36d
Ener-Getick <energetick.guigui@gmail.com>
2014-10-12 15:06:12 +0200

    Fix a bug

--- src/XN/UtilityBundle/TOTPAuthenticationListener.php
+++ src/XN/UtilityBundle/TOTPAuthenticationListener.php
@@ -13,7 +13,7 @@ class TOTPAuthenticationListener
 	private $em;
 	private $maxTrials;
 
-	private static $lut = [
+	public static $lut = [
 		"A" => 0,	"B" => 1,
 		"C" => 2,	"D" => 3,
 		"E" => 4,	"F" => 5,
