feda5692c668b188e6f7597d9813bdb8e25e0c5b
Ener-Getick <energetick.guigui@gmail.com>
2014-10-26 19:16:39 +0100

    test

--- app/memoclosure.sh
+++ app/memoclosure.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 APPDIR="$(dirname "$(dirname "$0")")"
-SHA224="$(sha224sum -t "$1" | head -c 56)"
+MD5="$(md5 -t "$1" | head -c 56)"
 TMPDIR="$APPDIR/tmp/build/memoclosure"
 
 mkdir -p "$TMPDIR"
 
-if [ -f "$TMPDIR/$SHA224.min.js" ]; then
-	cat "$TMPDIR/$SHA224.min.js"
+if [ -f "$TMPDIR/$MD5.min.js" ]; then
+	cat "$TMPDIR/$MD5.min.js"
 else
-	/usr/bin/java -jar /var/lib/closure-compiler/compiler.jar --language_in ECMASCRIPT5 "$1" | tee "$TMPDIR/$SHA224.min.js"
+	/usr/bin/java -jar /var/lib/closure-compiler/compiler.jar --language_in ECMASCRIPT5 "$1" | tee "$TMPDIR/$MD5.min.js"
 fi
