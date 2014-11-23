a04a6c7b1b04e1ad9c5f74469906519b614747f8
Ener-Getick <energetick.guigui@gmail.com>
2014-11-23 20:54:34 +0100

    test

--- src/Dof/CharactersBundle/Controller/DefaultController.php
+++ src/Dof/CharactersBundle/Controller/DefaultController.php
@@ -6,8 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($id)
     {
-        return $this->render('DofCharactersBundle:Default:index.html.twig', array('name' => $name));
+        $em = $this->getDoctrine()->getManager();
+        $spellRank = $em->getRepository('DofCharactersBundle:SpellRank')->findOneById($id);
+        return $this->render('DofCharactersBundle:Default:index.html.twig', array('spell' => $spellRank));
     }
 }
