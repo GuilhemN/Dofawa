306b42e72921fb3621132c1b3d8a00ea06d577a3
Ener-Getick <energetick.guigui@gmail.com>
2014-11-24 18:55:18 +0100

    Create a spell template

---
+++ src/Dof/CharactersBundle/Controller/SpellController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Dof\CharactersBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Dof\CharactersBundle\Entity\Spell;
+
+class SpellController extends Controller
+{
+    public function showAction(Spell $spell)
+    {
+        if(!$spell->isPubliclyVisible() && !$this->get('security.context')->isGranted('ROLE_SPELL_XRAY'))
+            throw $this->createNotFoundException();
+
+        return $this->render('DofCharactersBundle:Spell:show.html.twig', array('spell' => $spell));
+    }
+}
