f585a86a56e338f53042aac046d939d9b914b6bf
Ener-Getick <energetick.guigui@gmail.com>
2014-11-26 23:08:03 +0100

    test

--- src/XN/UtilityBundle/Twig/UtilityExtension.php
+++ src/XN/UtilityBundle/Twig/UtilityExtension.php
@@ -109,7 +109,7 @@ class UtilityExtension extends \Twig_Extension
 
 	public function getClassName($class){
 		$nClass = get_class($class);
-		$pos = strrchr($nClass, '\\');
+		$pos = strrpos($nClass, '\\');
 		return ($pos === false) ? $nClass : substr($nClass, $pos + 1);
 	}
 }
