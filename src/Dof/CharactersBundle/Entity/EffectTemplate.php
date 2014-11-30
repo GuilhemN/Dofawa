e2c074e659fe9543629c505cbee4b34f68a7b940
Exter-N <exter-n@exter-n.fr>
2014-11-30 13:51:25 +0100

    Expressions to fix effect display

--- src/Dof/CharactersBundle/Entity/EffectTemplate.php
+++ src/Dof/CharactersBundle/Entity/EffectTemplate.php
@@ -16,6 +16,8 @@ use Dof\ItemsBundle\ReleaseBoundTrait;
 
 use Dof\Common\GameTemplateString;
 
+use Dof\CharactersBundle\EffectTemplateExpressionLanguage;
+
 /**
  * EffectTemplate
  *
@@ -54,6 +56,42 @@ class EffectTemplate implements IdentifiableInterface, TimestampableInterface
      * @ORM\Column(name="operator", type="integer", nullable=true)
      */
     private $operator;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="param1_expression", type="string", nullable=true)
+	 */
+	private $param1Expression;
+
+	/**
+	 * @var callable
+	 */
+	private $compiledParam1Expression;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="param2_expression", type="string", nullable=true)
+	 */
+	private $param2Expression;
+
+	/**
+	 * @var callable
+	 */
+	private $compiledParam2Expression;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="param3_expression", type="string", nullable=true)
+	 */
+	private $param3Expression;
+
+	/**
+	 * @var callable
+	 */
+	private $compiledParam3Expression;
 
 	/**
 	 * @var Collection
@@ -160,6 +198,116 @@ class EffectTemplate implements IdentifiableInterface, TimestampableInterface
     }
 
     /**
+     * Set param1Expression
+     *
+     * @param string $param1Expression
+     * @return EffectTemplate
+     */
+    public function setParam1Expression($param1Expression)
+    {
+        $this->param1Expression = $param1Expression;
+		$this->compiledParam1Expression = null;
+
+        return $this;
+    }
+
+    /**
+     * Get param1Expression
+     *
+     * @return string
+     */
+    public function getParam1Expression()
+    {
+        return $this->param1Expression;
+    }
+
+    /**
+     * Set param2Expression
+     *
+     * @param string $param2Expression
+     * @return EffectTemplate
+     */
+    public function setParam2Expression($param2Expression)
+    {
+        $this->param2Expression = $param2Expression;
+		$this->compiledParam2Expression = null;
+
+        return $this;
+    }
+
+    /**
+     * Get param2Expression
+     *
+     * @return string
+     */
+    public function getParam2Expression()
+    {
+        return $this->param2Expression;
+    }
+
+    /**
+     * Set param3Expression
+     *
+     * @param string $param3Expression
+     * @return EffectTemplate
+     */
+    public function setParam3Expression($param3Expression)
+    {
+        $this->param3Expression = $param3Expression;
+		$this->compiledParam3Expression = null;
+
+        return $this;
+    }
+
+    /**
+     * Get param3Expression
+     *
+     * @return string
+     */
+    public function getParam3Expression()
+    {
+        return $this->param3Expression;
+    }
+
+	private function compileParamExpression($expression, &$compiledExpression, $defaultFunction)
+	{
+		if ($compiledExpression)
+			return;
+		$expression = trim(strval($expression));
+		if (!$expression) {
+			$compiledExpression = [ __CLASS__, $defaultFunction ];
+			return;
+		}
+		switch ($expression) {
+			case 'param1':
+				$compiledExpression = [ __CLASS__, 'selectParam1' ];
+				break;
+			case 'param2':
+				$compiledExpression = [ __CLASS__, 'selectParam2' ];
+				break;
+			case 'param3':
+				$compiledExpression = [ __CLASS__, 'selectParam3' ];
+				break;
+			default:
+				$el = new EffectTemplateExpressionLanguage();
+				$compiledExpression = eval('return function ($param1, $param2, $param3) { return ' . $el->compile($expression, [ 'param1', 'param2', 'param3' ]) . '; };');
+				break;
+		}
+	}
+	private static function selectParam1($param1, $param2, $param3)
+	{
+		return $param1;
+	}
+	private static function selectParam2($param1, $param2, $param3)
+	{
+		return $param2;
+	}
+	private static function selectParam3($param1, $param2, $param3)
+	{
+		return $param3;
+	}
+
+    /**
      * Add relations
      *
      * @param EffectTemplateRelation $relations
@@ -199,7 +347,19 @@ class EffectTemplate implements IdentifiableInterface, TimestampableInterface
 	{
 		$tpl = $this->getDescription($locale);
 		if ($tpl === null)
-			return null;
-		return GameTemplateString::expand($tpl, $context);
+			return [ ];
+		$ctx2 = $context + [
+			1 => null,
+			2 => null,
+			3 => null
+		];
+		$this->compileParamExpression($this->param1Expression, $this->compiledParam1Expression, 'selectParam1');
+		$this->compileParamExpression($this->param2Expression, $this->compiledParam2Expression, 'selectParam2');
+		$this->compileParamExpression($this->param3Expression, $this->compiledParam3Expression, 'selectParam3');
+		return GameTemplateString::expand($tpl, [
+			1 => call_user_func($this->compiledParam1Expression, $ctx2[1], $ctx2[2], $ctx2[3]),
+			2 => call_user_func($this->compiledParam2Expression, $ctx2[1], $ctx2[2], $ctx2[3]),
+			3 => call_user_func($this->compiledParam3Expression, $ctx2[1], $ctx2[2], $ctx2[3])
+		]);
 	}
 }
