8c4df8795eef19e75707e0926d700a9bb93b896b
Ener-Getick <energetick.guigui@gmail.com>
2014-12-03 20:12:18 +0100

    Fix a bug

--- src/Dof/ImpExpBundle/Importer/GameData/SpellRankEffectImporter.php
+++ src/Dof/ImpExpBundle/Importer/GameData/SpellRankEffectImporter.php
@@ -35,9 +35,9 @@ class SpellRankEffectImporter extends AbstractGameDataImporter
 
         $effects = array();
         foreach($nEffects as $row)
-            $ranksEffects[$row['id']][] = $row + ['critical' => false];
+            $ranksEffects[$row['id']]['normal'][] = $row + ['critical' => false];
         foreach($cEffects as $row)
-            $ranksEffects[$row['id']][] = $row + ['critical' => true];
+            $ranksEffects[$row['id']]['critical'][] = $row + ['critical' => true];
 
         $repo = $this->dm->getRepository('DofCharactersBundle:SpellRankEffect');
         $rankRepo = $this->dm->getRepository('DofCharactersBundle:SpellRank');
@@ -52,29 +52,53 @@ class SpellRankEffectImporter extends AbstractGameDataImporter
             if($rank === null || ($rank->getSpell()->isPreliminary() ^ $beta))
                 continue;
 
-            CollectionSynchronizationHelper::synchronize($this->dm, $rank->getEffects()->toArray(), $effects, function () use ($rank) {
-                $effect = new SpellRankEffect();
-                $effect->setSpellRank($rank);
-                return $effect;
-            }, function ($effect, $row) use($effectRepo){
-                $effectTemplate = $effectRepo->find($row['effectId']);
-                $effect->setEffectTemplate($effectTemplate);
+                CollectionSynchronizationHelper::synchronize($this->dm, $rank->getEffects()->toArray(), $effects['normal'], function () use ($rank) {
+                    $effect = new SpellRankEffect();
+                    $effect->setSpellRank($rank);
+                    return $effect;
+                }, function ($effect, $row) use($effectRepo){
+                    $effectTemplate = $effectRepo->find($row['effectId']);
+                    $effect->setEffectTemplate($effectTemplate);
 
-                $effect->setOrder($row['_index1']);
-                $effect->setProbability($row['random'] / 100);
-                $effect->setAreaOfEffect($row['rawZone']);
-                $effect->setTargets(explode(',', $row['targetMask']));
-                $effect->setDuration($row['duration']);
-                $effect->setDelay($row['delay']);
-                $effect->setTriggers(explode('|', $row['triggers']));
-                $effect->setHidden($row['hidden']);
-                $effect->setCritical($row['critical']);
+                    $effect->setOrder($row['_index1']);
+                    $effect->setProbability($row['random'] / 100);
+                    $effect->setAreaOfEffect($row['rawZone']);
+                    $effect->setTargets(explode(',', $row['targetMask']));
+                    $effect->setDuration($row['duration']);
+                    $effect->setDelay($row['delay']);
+                    $effect->setTriggers(explode('|', $row['triggers']));
+                    $effect->setHidden($row['hidden']);
+                    $effect->setCritical($row['critical']);
 
-                $effect->setParam1($row['diceNum']);
-                $effect->setParam2($row['diceSide']);
-                $effect->setParam3($row['value']);
+                    $effect->setParam1($row['diceNum']);
+                    $effect->setParam2($row['diceSide']);
+                    $effect->setParam3($row['value']);
 
-            });
+                });
+
+                CollectionSynchronizationHelper::synchronize($this->dm, $rank->getEffects()->toArray(), $effects['critical'], function () use ($rank) {
+                    $effect = new SpellRankEffect();
+                    $effect->setSpellRank($rank);
+                    return $effect;
+                }, function ($effect, $row) use($effectRepo){
+                    $effectTemplate = $effectRepo->find($row['effectId']);
+                    $effect->setEffectTemplate($effectTemplate);
+
+                    $effect->setOrder($row['_index1']);
+                    $effect->setProbability($row['random'] / 100);
+                    $effect->setAreaOfEffect($row['rawZone']);
+                    $effect->setTargets(explode(',', $row['targetMask']));
+                    $effect->setDuration($row['duration']);
+                    $effect->setDelay($row['delay']);
+                    $effect->setTriggers(explode('|', $row['triggers']));
+                    $effect->setHidden($row['hidden']);
+                    $effect->setCritical($row['critical']);
+
+                    $effect->setParam1($row['diceNum']);
+                    $effect->setParam2($row['diceSide']);
+                    $effect->setParam3($row['value']);
+
+                });
 
             ++$rowsProcessed;
             if (($rowsProcessed % 150) == 0) {
