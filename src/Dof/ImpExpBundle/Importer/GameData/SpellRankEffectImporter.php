2d9a4de7d76aa83d5cedb0e6c2dac8a5f4d3e835
Ener-Getick <energetick.guigui@gmail.com>
2014-12-04 21:09:44 +0100

    Change a condition

--- src/Dof/ImpExpBundle/Importer/GameData/SpellRankEffectImporter.php
+++ src/Dof/ImpExpBundle/Importer/GameData/SpellRankEffectImporter.php
@@ -49,9 +49,7 @@ class SpellRankEffectImporter extends AbstractGameDataImporter
 
         foreach ($ranksEffects as $rank => $effects) {
             $rank = $rankRepo->find($rank);
-            if($rank === null || ($rank->getSpell()->isPreliminary() ^ $beta))
-                continue;
-
+            if($rank !== null && ($rank->getSpell()->isPreliminary() ^ $beta)){
                 if(isset($effects['normal']))
                 CollectionSynchronizationHelper::synchronize($this->dm, array_values($rank->getNormalEffects()), $effects['normal'], function () use ($rank) {
                     $effect = new SpellRankEffect();
@@ -100,6 +98,7 @@ class SpellRankEffectImporter extends AbstractGameDataImporter
                     $effect->setParam3($row['value']);
 
                 });
+            }
 
             ++$rowsProcessed;
             if (($rowsProcessed % 150) == 0) {
