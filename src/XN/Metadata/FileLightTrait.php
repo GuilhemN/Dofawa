97a0570ecd8eaf6a2a41cb7a9bc25aff44ad46a7
Ener-Getick <energetick.guigui@gmail.com>
2014-12-06 17:42:20 +0100

    [...]

--- src/XN/Metadata/FileLightTrait.php
+++ src/XN/Metadata/FileLightTrait.php
@@ -19,17 +19,6 @@ trait FileLightTrait
     */
     protected $uploadIndex;
 
-    /**
-    * @Assert\Image(
-    *     maxSize = "1024k",
-    *     minWidth = 80,
-    *     maxWidth = 135,
-    *     minHeight = 80,
-    *     maxHeight = 135,
-    *     mimeTypesMessage = "Choisissez un fichier image valide.")
-    */
-    protected $file;
-
     private $pathToRemove;
 
     public function getPath(){
