354a0e88c5246e8e5fcc82f0a70ed079c811a6f1
menthosaire <sebastien.michon17@gmail.com>
2014-12-06 15:50:23 +0100

    Test

--- src/Dof/MainBundle/Controller/DefaultController.php
+++ src/Dof/MainBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
 		$em = $this->getDoctrine()->getManager();
 		$articles = $em->getRepository('DofArticlesBundle:Article')->findArticlesWithLimits(4);
 
-		foreach ($articles as $k => $article) {
+		foreach ($articles as $k => &$article) {
 			$content = $article->getDescription($translator->getLocales());
 			$article->setDescription(preg_replace('/<img[^>]>/', '', $content), $translator->getLocales());
 		} 
