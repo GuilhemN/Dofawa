67b18894160cda14b71ca96b159d2067f83827f4
Ener-Getick <energetick.guigui@gmail.com>
2014-12-06 12:24:21 +0100

    [...]

--- src/XN/Metadata/FileInterface.php
+++ src/XN/Metadata/FileInterface.php
@@ -9,9 +9,6 @@ interface FileInterface
     public function getAbsolutePath();
     public function getWebPath();
 
-    public function setFile(UploadedFile $file = null);
-    public function getFile();
-
     public function preUpload();
     public function upload();
     public function removeUpload();
