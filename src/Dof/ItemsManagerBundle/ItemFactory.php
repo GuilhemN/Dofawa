710520423b3a30c3a4773a99a5bcfa40c4be464b
Ener-Getick <energetick.guigui@gmail.com>
2014-12-08 22:48:46 +0100

    Create an item factory

---
+++ src/Dof/ItemsManagerBundle/ItemFactory.php
@@ -0,0 +1,64 @@
+<?php
+namespace Dof\ItemsManagerBundle;
+
+use Symfony\Component\Security\Core\SecurityContext;
+
+use Dof\UserBundle\Entity\User;
+use Dof\ItemsBundle\Entity\EquipmentTemplate;
+use Dof\ItemsManagerBundle\Entity as Ent;
+
+use Dof\ItemsBundle\ItemSlot;
+use Dof\BuildBundle\BuildSlot;
+
+class PersonalizedItemFactory {
+
+    /**
+    * @var Symfony\Component\Security\Core\SecurityContext
+    */
+    private $sc;
+
+    public function __construct(SecurityContext $sc) {
+        $this->sc = $sc;
+    }
+
+    public function createItem(EquipmentTemplate $item, $percent = null, User $user = null){
+        if($user === null)
+            $user = $this->sc->getToken()->getUser();
+        if(!($user instanceOf User))
+            return;
+        if($percent === null)
+            $percent = 80;
+        $percent = intval($percent);
+
+
+        switch ($item->geType()->getSlot()) {
+            case ItemSlot::WEAPON:
+            $ent = new Ent\Weapon();
+            case ItemSlot::PET:
+            switch ($item->getType()->getId()) {
+                case 121: // Petsmount
+                $ent = new Ent\Mount();
+                default:
+                $ent = new Ent\Pet();
+            }
+            case ItemSlot::MOUNT:
+            $ent = new Ent\Mount();
+            default:
+            $ent = new Ent\Item();
+        }
+
+        $ent->setOwner($user);
+        $ent->setItemTemplate($itemTemplate);
+        $ent->setSlot(BuildSlot::getBuildSlot($item->getType()->getSlot())[0]);
+
+        $caracts = $item->getCharacteristics();
+        foreach($caracts as $k => &$caract){
+            $min = $item->{'getMin' . ucfirst($k)}();
+            $max = $item->{'getMax' . ucfirst($k)}();
+            $caract = round($min + ($max - $min) * $percent / 100);
+        }
+        $ent->setCharacteristics($caracts, true);
+
+        return $ent;
+    }
+}
