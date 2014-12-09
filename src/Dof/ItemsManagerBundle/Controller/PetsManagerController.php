57b219196e402f3fe3dda35b3b3d41ac25b1d5cb
menthosaire <sebastien.michon17@gmail.com>
2014-12-09 18:44:05 +0100

    Add controller for PetsManager

---
+++ src/Dof/ItemsManagerBundle/Controller/PetsManagerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dof\ItemsManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Dof\ItemsBundle\Entity\ItemTemplate;
+
+class PetsManagerController extends Controller
+{
+    public function showAction()
+    {
+    	if(!$this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+            throw $this->createAccessDeniedException();
+
+        // plus tard
+
+        return $this->render('DofItemsManagerBundle:PetsManager:show.html.twig', array('name' => $name));
+    }
+
+    public function addAction()
+    {
+    	if(!$this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+            throw $this->createAccessDeniedException();
+
+    	$repository = $this->getDoctrine()->getRepository('DofItemsManagerBundle:Pet');
+    	$petsList[] = $repository->getPetsList();
+
+        return $this->render('DofItemsManagerBundle:PetsManager:add.html.twig', array('name' => $name));
+    }
+}
