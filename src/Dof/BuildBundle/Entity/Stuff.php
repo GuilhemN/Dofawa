7b25eb08aee8fe4c49011c2c8f4031df97b43c0f
Ener-Getick <energetick.guigui@gmail.com>
2014-12-09 18:51:17 +0100

    test

--- src/Dof/BuildBundle/Entity/Stuff.php
+++ src/Dof/BuildBundle/Entity/Stuff.php
@@ -932,53 +932,63 @@ class Stuff implements IdentifiableInterface, TimestampableInterface, SluggableI
         $slot = intval($slot);
         switch ($item->getItemTemplate()->getType()->getSlot()) {
             case ItemSlot::AMULET:
-            $lItem = $this->getAmulet();
-            $this->setAmulet($item);
+                $lItem = $this->getAmulet();
+                $this->setAmulet($item);
+                break;
             case ItemSlot::WEAPON:
-            $lItem = $this->getWeapon();
-            $this->setWeapon($item);
-            $this->getLook()->setWeapon($item->getItemTemplate());
+                $lItem = $this->getWeapon();
+                $this->setWeapon($item);
+                $this->getLook()->setWeapon($item->getItemTemplate());
+                break;
             case ItemSlot::RING:
-            if($slot == 2){
-                $lItem = $this->getRing2();
-                $this->setRing2($item);
-            }
-            else{
-                $lItem = $this->getRing1();
-                $this->setRing1($item);
-            }
+                if($slot == 2){
+                    $lItem = $this->getRing2();
+                    $this->setRing2($item);
+                }
+                else{
+                    $lItem = $this->getRing1();
+                    $this->setRing1($item);
+                }
+                break;
             case ItemSlot::BELT:
-            $lItem = $this->getBelt();
-            $this->setBelt($item);
+                $lItem = $this->getBelt();
+                $this->setBelt($item);
+                break;
             case ItemSlot::BOOTS:
-            $lItem = $this->getBoots();
-            $this->setBoots($item);
+                $lItem = $this->getBoots();
+                $this->setBoots($item);
+                break;
             case ItemSlot::SHIELD:
-            $lItem = $this->getShield();
-            $this->setShield($item);
-            $this->getLook()->setShield($item->getItemTemplate());
+                $lItem = $this->getShield();
+                $this->setShield($item);
+                $this->getLook()->setShield($item->getItemTemplate());
+                break;
             case ItemSlot::HAT:
-            $lItem = $this->getHat();
-            $this->setHat($item);
-            $this->getLook()->setHat($item->getItemTemplate());
+                $lItem = $this->getHat();
+                $this->setHat($item);
+                $this->getLook()->setHat($item->getItemTemplate());
+                break;
             case ItemSlot::CLOAK:
-            $lItem = $this->getCloak();
-            $this->setCloak($item);
-            $this->getLook()->setCloak($item->getItemTemplate());
+                $lItem = $this->getCloak();
+                $this->setCloak($item);
+                $this->getLook()->setCloak($item->getItemTemplate());
+                break;
             case ItemSlot::PET:
             case ItemSlot::MOUNT:
-            $lItem = $this->getAnimal();
-            $this->setAnimal($item);
-            $this->getLook()->setAnimal($item->getItemTemplate());
+                $lItem = $this->getAnimal();
+                $this->setAnimal($item);
+                $this->getLook()->setAnimal($item->getItemTemplate());
+                break;
             case ItemSlot::DOFUS:
-            if($slot >= 1 && $slot <= 6){
-                $lItem = $this->{ 'getDofus' . $slot }();
-                $this->{ 'setDofus' . $slot }($item);
-            }
-            else{
-                $lItem = $this->getDofus1();
-                $this->setDofus1($item);
-            }
+                if($slot >= 1 && $slot <= 6){
+                    $lItem = $this->{ 'getDofus' . $slot }();
+                    $this->{ 'setDofus' . $slot }($item);
+                }
+                else{
+                    $lItem = $this->getDofus1();
+                    $this->setDofus1($item);
+                }
+                break;
         }
         return $lItem;
     }
