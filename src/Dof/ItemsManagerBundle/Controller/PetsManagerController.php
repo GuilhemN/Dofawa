800ff40994ef1a4426af3c56ac7874e9f10fc6dd
menthosaire <sebastien.michon17@gmail.com>
2014-12-10 22:05:05 +0100

    Add feed pets

--- src/Dof/ItemsManagerBundle/Controller/PetsManagerController.php
+++ src/Dof/ItemsManagerBundle/Controller/PetsManagerController.php
@@ -25,4 +25,23 @@ class PetsManagerController extends Controller
 
         return $this->render('DofItemsManagerBundle:PetsManager:add.html.twig', array('name' => $name));
     }
+
+    public function feedAction()
+    {
+        if(!$this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+            throw $this->createAccessDeniedException();
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository('DofItemsManagerBundle:Pet');
+        $pets = $repository->getRaisablePets($this->getUser());
+        $petsFeed = $repository->findBy(['id' => $this->get('request')->get('pets'), 'owner' => $this->getUser()]);
+
+        foreach ($petsFeed as $pet)
+            if($pet->isRaise())
+                $pet->setLastMeal(date('Y-m-d H:i:s'));
+                
+        $em->flush();
+
+        return $this->render('DofItemsManagerBundle:PetsManager:show.html.twig', array('pets' => $pets));
+    }
 }
