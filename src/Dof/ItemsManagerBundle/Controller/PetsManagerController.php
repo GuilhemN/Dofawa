ce2fa94007eab34377987101de8e09e52ea145b0
menthosaire <sebastien.michon17@gmail.com>
2014-12-13 12:27:15 +0100

    Test

--- src/Dof/ItemsManagerBundle/Controller/PetsManagerController.php
+++ src/Dof/ItemsManagerBundle/Controller/PetsManagerController.php
@@ -5,6 +5,8 @@ namespace Dof\ItemsManagerBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Dof\ItemsBundle\Entity\ItemTemplate;
 
+use Dof\ItemsManagerBundle\Entity\PersonalizedItem;
+
 class PetsManagerController extends Controller
 {
     public function showAction()
@@ -18,12 +20,30 @@ class PetsManagerController extends Controller
         return $this->render('DofItemsManagerBundle:PetsManager:show.html.twig', array('pets' => $pets));
     }
 
-    public function addAction()
+    public function addAction($id)
     {
     	if(!$this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
             throw $this->createAccessDeniedException();
+        $iFact = $this->get('item_factory');
 
-        return $this->render('DofItemsManagerBundle:PetsManager:add.html.twig', array('name' => $name));
+        $item = $em->getRepository('DofItemsBundle:ItemTemplate')->findById($id);
+        $em = $this->getDoctrine()->getManager();
+
+        if($item->getType() == 18)
+        {
+            $pet = $iFact->createItem($item, null, $this->getUser());
+            $pet->setRaise(true);
+            $pet->setKeep(true);
+            $em->persist($iFact);
+            $em->flush();
+        }
+
+        $repository = $this->getDoctrine()->getRepository('DofItemsManagerBundle:Pet');
+        $pets = $repository->getRaisablePets($this->getUser());
+
+        return $this->redirect($this->generateUrl('dof_items_manager_pets', array(
+                'pets' => $pets
+                )));
     }
 
     public function feedAction()
