7944a873fb1b319b43a9dab78896014c13db85a2
Ener-Getick <energetick.guigui@gmail.com>
2014-12-13 14:18:19 +0100

    test

--- src/Dof/QuestBundle/Entity/QuestObjective.php
+++ src/Dof/QuestBundle/Entity/QuestObjective.php
@@ -166,6 +166,6 @@ class QuestObjective implements IdentifiableInterface
     }
 
     public function __toString(){
-        return $this->step->getQuest();
+        return $this->step->getQuest()->getNameFr();
     }
 }
