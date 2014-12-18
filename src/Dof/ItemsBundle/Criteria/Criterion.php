afed0e0aa5706860cbb39244f39015771602449b
Ener-Getick <energetick.guigui@gmail.com>
2014-12-18 21:56:14 +0100

    [...]

--- src/Dof/ItemsBundle/Criteria/Criterion.php
+++ src/Dof/ItemsBundle/Criteria/Criterion.php
@@ -3,4 +3,7 @@ namespace Dof\ItemsBundle\Criteria;
 
 class Criterion
 {
+    public function getVisible() {
+        return true;
+    }
 }
--- src/Dof/ItemsBundle/Criteria/ParsedCriteriaInterface.php
+++ src/Dof/ItemsBundle/Criteria/ParsedCriteriaInterface.php
@@ -5,7 +5,7 @@ namespace Dof\ItemsBundle\Criteria;
 interface ParsedCriteriaInterface
 {
     public function getParsedCriteria();
-    
-    public function setTreatedCriteria(array $treatedCriteria);
+
+    public function setTreatedCriteria($treatedCriteria);
     public function getTreatedCriteria();
 }
