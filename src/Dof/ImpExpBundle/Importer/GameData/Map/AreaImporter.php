233ebc78c33f95c9c576272dec5b8ed2ad1bb1d1
Ener-Getick <energetick.guigui@gmail.com>
2014-12-19 18:55:33 +0100

    [...]

--- src/Dof/ImpExpBundle/Importer/GameData/Map/AreaImporter.php
+++ src/Dof/ImpExpBundle/Importer/GameData/Map/AreaImporter.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Dof\ImpExpBundle\Importer\GameData\AbstractGameDataImporter;
 use Dof\ImpExpBundle\ImporterFlags;
 
-use Dof\MapBundle\Area;
+use Dof\MapBundle\Entity\Area;
 
 class AreaImporter extends AbstractGameDataImporter
 {
--- src/Dof/ImpExpBundle/Importer/GameData/Map/SuperAreaImporter.php
+++ src/Dof/ImpExpBundle/Importer/GameData/Map/SuperAreaImporter.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Dof\ImpExpBundle\Importer\GameData\AbstractGameDataImporter;
 use Dof\ImpExpBundle\ImporterFlags;
 
-use Dof\MapBundle\SuperArea;
+use Dof\MapBundle\Entity\SuperArea;
 
 class SuperAreaImporter extends AbstractGameDataImporter
 {
