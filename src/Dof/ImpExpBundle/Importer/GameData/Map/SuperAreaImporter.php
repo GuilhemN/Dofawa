c71306f636babd1eb3146ca4ee16db80d903a6f9
Ener-Getick <energetick.guigui@gmail.com>
2014-12-19 18:53:06 +0100

    Add two map areas importers

---
+++ src/Dof/ImpExpBundle/Importer/GameData/Map/SuperAreaImporter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Dof\ImpExpBundle\Importer\GameData\Map;
+
+use Symfony\Component\Console\Helper\ProgressHelper;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Dof\ImpExpBundle\Importer\GameData\AbstractGameDataImporter;
+use Dof\ImpExpBundle\ImporterFlags;
+
+use Dof\MapBundle\SuperArea;
+
+class QuestImporter extends AbstractGameDataImporter
+{
+    const CURRENT_DATA_SET = 'super_areas';
+    const BETA_DATA_SET = 'beta_super_areas';
+
+    protected function doImport($conn, $beta, $release, $db, array $locales, $flags, OutputInterface $output = null, ProgressHelper $progress = null)
+    {
+        $write = ($flags & ImporterFlags::DRY_RUN) == 0;
+        if (!$beta && $write)
+        $this->dm->createQuery('UPDATE DofMapBundle:SuperArea s SET s.deprecated = true')->execute();
+        $stmt = $conn->query('SELECT o.*' .
+        $this->generateD2ISelects('name', $locales) .
+        ' FROM ' . $db . '.D2O_SuperArea o' .
+        $this->generateD2IJoins('name', $db, $locales));
+        $all = $stmt->fetchAll();
+        $stmt->closeCursor();
+        $repo = $this->dm->getRepository('DofMapBundle:SuperArea');
+        $rowsProcessed = 0;
+        if ($output && $progress)
+        $progress->start($output, count($all));
+        foreach ($all as $row) {
+            $tpl = $repo->find($row['id']);
+            if ($tpl === null) {
+                $tpl = new SuperArea();
+                $tpl->setDeprecated(true);
+                $tpl->setId($row['id']);
+            }
+            if ($tpl->isDeprecated()) {
+                $tpl->setDeprecated(false);
+                if (!$tpl->getRelease())
+                $tpl->setRelease($release);
+                $tpl->setPreliminary($beta);
+
+                $this->copyI18NProperty($tpl, 'setName', $row, 'name');
+                $this->dm->persist($tpl);
+                $this->su->reassignSlug($tpl);
+            }
+            ++$rowsProcessed;
+            if (($rowsProcessed % 300) == 0) {
+                $this->dm->flush();
+                $this->dm->clear();
+                if ($output && $progress)
+                $progress->advance(300);
+            }
+        }
+        if ($output && $progress)
+        $progress->finish();
+
+    }
+}
