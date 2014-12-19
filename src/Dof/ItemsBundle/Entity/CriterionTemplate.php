017041bd16ac95b709bca7ba551ac241da71ddc0
Ener-Getick <energetick.guigui@gmail.com>
2014-12-19 18:00:46 +0100

    Show the french description for criteria if nothing other

--- src/Dof/ItemsBundle/Entity/CriterionTemplate.php
+++ src/Dof/ItemsBundle/Entity/CriterionTemplate.php
@@ -150,7 +150,7 @@ class CriterionTemplate implements IdentifiableInterface, TimestampableInterface
 
     public function expandDescription(array $context, $locale = 'fr')
     {
-        $tpl = $this->getDescription($locale);
+        $tpl = !empty($description = $this->getDescription($locale)) ? $description : $this->getDescription('fr');
         if ($tpl === null)
             return [ ];
         $context = $context + [
