4a157e8f793b1b74c914421998a103ee592cd727
Ener-Getick <energetick.guigui@gmail.com>
2014-12-21 16:55:45 +0100

    Add the maps

---
+++ src/Dof/ImpExpBundle/Importer/GameData/Map/PositionImporter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Dof\ImpExpBundle\Importer\GameData\Map;
+
+use Symfony\Component\Console\Helper\ProgressHelper;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Dof\ImpExpBundle\Importer\GameData\AbstractGameDataImporter;
+use Dof\ImpExpBundle\ImporterFlags;
+
+use Dof\MapBundle\Entity\MapPosition;
+
+class PositionImporter extends AbstractGameDataImporter
+{
+    const CURRENT_DATA_SET = 'map_positions';
+    const BETA_DATA_SET = 'beta_map_positions';
+
+    protected function doImport($conn, $beta, $release, $db, array $locales, $flags, OutputInterface $output = null, ProgressHelper $progress = null)
+    {
+        $write = ($flags & ImporterFlags::DRY_RUN) == 0;
+        if (!$beta && $write)
+            $this->dm->createQuery('UPDATE DofMapBundle:MapPosition s SET s.deprecated = true')->execute();
+        $stmt = $conn->query('SELECT o.*' .
+        $this->generateD2ISelects('name', $locales) .
+        ' FROM ' . $db . '.D2O_MapPosition o' .
+        $this->generateD2IJoins('name', $db, $locales));
+        $all = $stmt->fetchAll();
+        $stmt->closeCursor();
+        $repo = $this->dm->getRepository('DofMapBundle:MapPosition');
+        $subRepo = $this->dm->getRepository('DofMapBundle:SubArea');
+        $rowsProcessed = 0;
+        if ($output && $progress)
+        $progress->start($output, count($all));
+        foreach ($all as $row) {
+            $subArea = $subRepo->find($row['subAreaId']);
+            if($subArea === null)
+                continue;
+            $tpl = $repo->find($row['id']);
+            if ($tpl === null) {
+                $tpl = new MapPosition();
+                $tpl->setDeprecated(true);
+                $tpl->setId($row['id']);
+            }
+            if ($tpl->isDeprecated()) {
+                $tpl->setDeprecated(false);
+                if (!$tpl->getRelease())
+                    $tpl->setRelease($release);
+                $tpl->setPreliminary($beta);
+                $tpl->setSuperArea($superArea);
+                $tpl->setX($row['posX']);
+                $tpl->setY($row['posY']);
+                $tpl->setOutdoor($row['outdoor']);
+                $tpl->setCapabilities($row['capabilities']);
+                $tpl->setWorldMap($row['worldMap']);
+                $tpl->setHasPriorityOnWorldMap($row['hasPriorityOnWorldmap']);
+
+                $this->copyI18NProperty($tpl, 'setName', $row, 'name');
+                if($tpl->getNameFr() === null)
+                    $tpl->setNameFr('map');
+                $this->dm->persist($tpl);
+                $this->su->reassignSlug($tpl);
+            }
+            ++$rowsProcessed;
+            if (($rowsProcessed % 300) == 0) {
+                $this->dm->flush();
+                $this->dm->clear();
+                if ($output && $progress)
+                $progress->advance(300);
+            }
+        }
+        if ($output && $progress)
+        $progress->finish();
+
+    }
+}
---
+++ src/Dof/MapBundle/Entity/MapPosition.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace Dof\MapBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use XN\Persistence\IdentifiableInterface;
+use XN\Metadata\TimestampableInterface;
+use XN\Metadata\TimestampableTrait;
+
+# L'interface n'est pas utilisé afin que le __toString() soit utilisé et non le nom de l'entité
+# use XN\L10n\LocalizedNameInterface;
+use XN\L10n\LocalizedNameTrait;
+use Dof\ItemsBundle\ReleaseBoundTrait;
+
+/**
+ * MapPosition
+ *
+ * @ORM\Table(name="dof_map_positions")
+ * @ORM\Entity(repositoryClass="Dof\MapBundle\Entity\MapPositionRepository")
+ */
+class MapPosition implements IdentifiableInterface, TimestampableInterface
+{
+    use TimestampableTrait, LocalizedNameTrait, ReleaseBoundTrait;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="x", type="integer")
+     */
+    private $x;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="y", type="integer")
+     */
+    private $y;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="outdoor", type="boolean")
+     */
+    private $outdoor;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="capabilities", type="integer")
+     */
+    private $capabilities;
+
+    /**
+    * @var integer
+    *
+    * @ORM\Column(name="worldMap", type="integer")
+    */
+    private $worldMap;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="hasPriorityOnWorldMap", type="boolean")
+     */
+    private $hasPriorityOnWorldMap;
+
+    /**
+    * @var Monster
+    *
+    * @ORM\ManyToOne(targetEntity="SubArea", inversedBy="maps")
+    * @ORM\JoinColumn(nullable=false, onDelete="SET NULL")
+    */
+    private $subArea;
+
+    /**
+    * Set id
+    *
+    * @param integer $id
+    * @return MapPosition
+    */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set x
+     *
+     * @param integer $x
+     * @return MapPosition
+     */
+    public function setX($x)
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+
+    /**
+     * Get x
+     *
+     * @return integer
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * Set y
+     *
+     * @param integer $y
+     * @return MapPosition
+     */
+    public function setY($y)
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * Get y
+     *
+     * @return integer
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * Set outdoor
+     *
+     * @param boolean $outdoor
+     * @return MapPosition
+     */
+    public function setOutdoor($outdoor)
+    {
+        $this->outdoor = $outdoor;
+
+        return $this;
+    }
+
+    /**
+     * Get outdoor
+     *
+     * @return boolean
+     */
+    public function getOutdoor()
+    {
+        return $this->outdoor;
+    }
+
+    /**
+     * Set capabilities
+     *
+     * @param integer $capabilities
+     * @return MapPosition
+     */
+    public function setCapabilities($capabilities)
+    {
+        $this->capabilities = $capabilities;
+
+        return $this;
+    }
+
+    /**
+     * Get capabilities
+     *
+     * @return integer
+     */
+    public function getCapabilities()
+    {
+        return $this->capabilities;
+    }
+
+    /**
+    * Set worldMap
+    *
+    * @param integer $worldMap
+    * @return MapPosition
+    */
+    public function setWorldMap($worldMap)
+    {
+        $this->worldMap = $worldMap;
+
+        return $this;
+    }
+
+    /**
+    * Get worldMap
+    *
+    * @return integer
+    */
+    public function getWorldMap()
+    {
+        return $this->worldMap;
+    }
+
+    /**
+     * Set hasPriorityOnWorldMap
+     *
+     * @param boolean $hasPriorityOnWorldMap
+     * @return MapPosition
+     */
+    public function setHasPriorityOnWorldMap($hasPriorityOnWorldMap)
+    {
+        $this->hasPriorityOnWorldMap = $hasPriorityOnWorldMap;
+
+        return $this;
+    }
+
+    /**
+     * Get hasPriorityOnWorldMap
+     *
+     * @return boolean
+     */
+    public function getHasPriorityOnWorldMap()
+    {
+        return $this->hasPriorityOnWorldMap;
+    }
+
+    /**
+    * Set subArea
+    *
+    * @param SubArea $subArea
+    * @return MapPosition
+    */
+    public function setSubArea(SubArea $subArea)
+    {
+        $this->subArea = $subArea;
+
+        return $this;
+    }
+
+    /**
+    * Get subArea
+    *
+    * @return SubArea
+    */
+    public function getSubArea()
+    {
+        return $this->subArea;
+    }
+
+    public function __toString() {
+        return '[' . $this->x . ';' . $this->y . ']';
+    }
+}
