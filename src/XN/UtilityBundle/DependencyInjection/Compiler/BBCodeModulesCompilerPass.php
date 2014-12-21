96ddcaa5944c986728dde4302ea62844ca6afe02
Exter-N <exter-n@exter-n.fr>
2014-12-21 03:42:58 +0100

    BBCode service container extension

---
+++ src/XN/UtilityBundle/DependencyInjection/Compiler/BBCodeModulesCompilerPass.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace XN\UtilityBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+class BBCodeModulesCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+		$modules = [ ];
+
+        $taggedServices = $container->findTaggedServiceIds('xn.bbcode.publish');
+        foreach ($taggedServices as $id => $tagAttributes) {
+            foreach ($tagAttributes as $attributes) {
+				if (isset($attributes['module'])) {
+					$module = $attributes['module'];
+					if (!isset($modules[$module]))
+						$modules[$module] = [ ];
+					$modules[$module][] = $id;
+				}
+			}
+		}
+
+		$taggedServices = $container->findTaggedServiceIds('xn.bbcode.use');
+		foreach ($taggedServices as $id => $tagAttributes) {
+			$definition = $container->getDefinition($id);
+			foreach ($tagAttributes as $attributes) {
+				if (isset($attributes['module'])) {
+					$module = $attributes['module'];
+					if (isset($modules[$module]))
+						foreach ($modules[$module] as $service)
+							$definition->addMethodCall('registerTagTemplate', [
+								new Reference($service)
+							]);
+				} elseif (isset($attributes['service'])) {
+					$definition->addMethodCall('registerTagTemplate', [
+					    new Reference($attributes['service'])
+					]);
+				}
+			}
+		}
+    }
+}
--- src/XN/UtilityBundle/XNUtilityBundle.php
+++ src/XN/UtilityBundle/XNUtilityBundle.php
@@ -3,7 +3,16 @@
 namespace XN\UtilityBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use XN\UtilityBundle\DependencyInjection\Compiler\BBCodeModulesCompilerPass;
 
 class XNUtilityBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new BBCodeModulesCompilerPass());
+    }
 }
