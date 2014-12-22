e4a25c451d4d6654ace4c0786e69b99fc623b980
Ener-Getick <energetick.guigui@gmail.com>
2014-12-22 18:10:49 +0100

    test

--- src/Dof/MapBundle/Entity/MapPosition.php
+++ src/Dof/MapBundle/Entity/MapPosition.php
@@ -3,6 +3,8 @@
 namespace Dof\MapBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 use XN\Persistence\IdentifiableInterface;
 use XN\Metadata\TimestampableInterface;
