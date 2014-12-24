3919a5355aec93c3e53176b7c049a491e7790577
menthosaire <sebastien.michon17@gmail.com>
2014-12-24 12:00:04 +0100

    Fix bug in create build from breed presentation

--- src/Dof/ItemsManagerBundle/Entity/Pet.php
+++ src/Dof/ItemsManagerBundle/Entity/Pet.php
@@ -50,6 +50,7 @@ class Pet extends Animal
     public function __construct(){
         $this->lastMeal = new \DateTime('now');
         $this->lastNotification = new \DateTime('now');
+        $nextMeal = clone $lastMeal;
         $this->nextMeal = $nextMeal->modify('+' . $this->getItemTemplate()->getMinFeedInterval() . ' hour');
     }
     /**
