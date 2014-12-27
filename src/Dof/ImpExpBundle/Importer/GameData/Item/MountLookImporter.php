d9a301939e87504c63562c3dcb171a17fb4ebecf
Ener-Getick <energetick.guigui@gmail.com>
2014-12-27 15:59:05 +0100

    Create a new importer

---
+++ src/Dof/ImpExpBundle/Importer/GameData/Item/MountLookImporter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Dof\ImpExpBundle\Importer\GameData\Item;
+
+use Symfony\Component\Console\Helper\ProgressHelper;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Dof\ImpExpBundle\Importer\GameData\AbstractGameDataImporter;
+use Dof\ImpExpBundle\ImporterFlags;
+
+use Dof\GraphicsBundle\EntityLook;
+use Dof\ItemsBundle\AnimalColorizationType;
+
+class MountLookImporter extends AbstractGameDataImporter
+{
+    const CURRENT_DATA_SET = 'mount_looks';
+    const BETA_DATA_SET = 'beta_mount_looks';
+
+    private $loaders;
+
+    protected function doImport($conn, $beta, $release, $db, array $locales, $flags, OutputInterface $output = null, ProgressHelper $progress = null)
+    {
+        $this->loaders[0]->setEnabled(false);
+        $this->loaders[1]->setEnabled(false);
+
+        $stmt = $conn->query('SELECT o.*' .
+        $this->generateD2ISelects('name', $locales) .
+        ' FROM ' . $db . '.D2O_Mount o' .
+        $this->generateD2IJoins('name', $db, $locales));
+        $all = $stmt->fetchAll();
+        $stmt->closeCursor();
+        $repo = $this->dm->getRepository('DofItemsBundle:MountTemplate');
+        $rowsProcessed = 0;
+        if ($output && $progress)
+        $progress->start($output, count($all));
+        foreach ($all as $row) {
+            $tpl = $repo->findOneByNameFr($row['nameFr']);
+            if($tpl === null)
+                continue;
+
+            $look = new EntityLook($row['look']);
+            $tpl->setBone($look->getBone());
+            $tpl->setColors($look->getColors());
+            $tpl->setSkins($look->getSkins());
+            $tpl->setColorizationType(AnimalColorizationType::COLORS);
+
+            ++$rowsProcessed;
+            if (($rowsProcessed % 300) == 0) {
+                $this->dm->flush();
+                $this->dm->clear();
+                if ($output && $progress)
+                $progress->advance(300);
+            }
+        }
+        if ($output && $progress)
+            $progress->finish();
+
+        $this->loaders[0]->setEnabled(true);
+        $this->loaders[1]->setEnabled(true);
+    }
+
+    public function setLoaders($loaders){
+        $this->loaders = $loaders;
+    }
+}
