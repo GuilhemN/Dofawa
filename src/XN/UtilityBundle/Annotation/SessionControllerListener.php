ef2e6a3a5066b51d4fa67bf2c67355e17e23d94d
Ener-Getick <energetick.guigui@gmail.com>
2014-12-27 22:46:42 +0100

    [...]

--- src/XN/UtilityBundle/Annotation/SessionControllerListener.php
+++ src/XN/UtilityBundle/Annotation/SessionControllerListener.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Doctrine\Common\Annotations\Reader;
+use XN\UtilityBundle\ParameterBag;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class SessionControllerListener
@@ -13,12 +14,14 @@ class SessionControllerListener
     private $sc;
     private $se;
     private $re;
+    private $pb;
     private $di;
 
-    public function __construct(SecurityContext $sc, Session $se, Reader $re, ContainerInterface $di){
+    public function __construct(SecurityContext $sc, Session $se, Reader $re, ParameterBag $pb, ContainerInterface $di){
         $this->sc = $sc;
         $this->se = $se;
         $this->re = $re;
+        $this->pb = $pb;
         $this->di = $di;
     }
 
@@ -32,8 +35,8 @@ class SessionControllerListener
             return;
 
         $session = $this->di->get('session');
-        $this->di->setParameter('dof_user.last_username', (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME));
-        $this->di->setParameter('dof_user.csrf_authenticate', $this->di->get('form.csrf_provider')->generateCsrfToken('authenticate'));
+        $this->pb->setParameter('dof_user.last_username', (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME));
+        $this->pb->setParameter('dof_user.csrf_authenticate', $this->di->get('form.csrf_provider')->generateCsrfToken('authenticate'));
 
         $token = $this->sc->getToken();
         if($token)
