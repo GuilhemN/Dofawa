3f4c41f00498c066cb5b1aac59756fa14263dae2
Ener-Getick <energetick.guigui@gmail.com>
2014-12-31 00:45:18 +0100

    Fix a bug

--- src/XN/UtilityBundle/Annotations/LazyFieldListener.php
+++ src/XN/UtilityBundle/Annotations/LazyFieldListener.php
@@ -26,19 +26,21 @@ class LazyFieldListener
         $ent = $args->getEntity();
         $class = $em->getClassMetadata(get_class($ent))->getName();
 
-        //Properties
-        if ($propertiesString = $this->ca->fetch('xn-class-properties/' . $class)) {
-            $properties = unserialize($propertiesString);
-        } else {
-            $reflect = new \ReflectionClass($ent);
-            $properties = array_map(function($v){
-                return $v->getName();
-            }, $reflect->getProperties());
-            $this->ca->save('xn-class-properties/' . $class, serialize($properties));
-        }
+
         if ($lazyFieldsString = $this->ca->fetch('xn-lazy-fields/' . $class)) {
             $lazyFields = unserialize($lazyFieldsString);
         } else {
+            //Properties
+            if ($propertiesString = $this->ca->fetch('xn-class-properties/' . $class)) {
+                $properties = unserialize($propertiesString);
+            } else {
+                $reflect = new \ReflectionClass($ent);
+                $properties = array_map(function($v){
+                    return $v->getName();
+                }, $reflect->getProperties());
+                $this->ca->save('xn-class-properties/' . $class, serialize($properties));
+            }
+            
             //Lazy Fields
             $lazyFields = [];
             foreach($properties as $property){
@@ -46,9 +48,8 @@ class LazyFieldListener
                 if($annotation)
                     $lazyFields[$property] = $annotation;
             }
-            $this->ca->save('xn-lazy-fields/' . $class, serialize($properties));
+            $this->ca->save('xn-lazy-fields/' . $class, serialize($lazyFields));
         }
-        throw new \Exception(var_dump($lazyFields));
 
         foreach($lazyFields as $k => $v) {
             $classMethod = !empty($v->classMethod) ? $v->classMethod : 'getClass';
