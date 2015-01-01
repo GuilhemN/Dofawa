356371b526b8b38d461488ed68bbdda870446c74
Ener-Getick <energetick.guigui@gmail.com>
2015-01-01 23:38:48 +0100

    [...]

--- src/Dof/CMSBundle/Entity/Article.php
+++ src/Dof/CMSBundle/Entity/Article.php
@@ -30,8 +30,6 @@ use XN\L10n\LocalizedDescriptionTrait;
  */
 class Article implements IdentifiableInterface, TimestampableInterface, SluggableInterface, OwnableInterface, LocalizedNameInterface
 {
-    const NULLABLE_NAME_FR = true;
-
     /**
      * @var integer
      *
