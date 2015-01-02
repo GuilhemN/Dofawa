8dfe163a197fd8e571a4311cc61ff37f369c6497
Ener-Getick <energetick.guigui@gmail.com>
2015-01-02 16:52:54 +0100

    debug

--- src/XN/UtilityBundle/Twig/UtilityExtension.php
+++ src/XN/UtilityBundle/Twig/UtilityExtension.php
@@ -138,7 +138,7 @@ class UtilityExtension extends \Twig_Extension
 
 		$doc = $lg->parse($source);
 		$htmlDoc = new \DOMDocument();
-		throw new \Exception(print_r($doc->toDOMNode($htmlDoc)));
+		throw new \Exception(print_r($doc->toDOMNode($htmlDoc),true));
 		return $lg->convertToHTML($source);
 	}
 }
