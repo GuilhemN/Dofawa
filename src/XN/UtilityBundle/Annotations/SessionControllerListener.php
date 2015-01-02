17138441e63134e21c05105f3210eb20ef1339f8
Ener-Getick <energetick.guigui@gmail.com>
2015-01-02 17:48:10 +0100

    Fix a bug

--- src/XN/UtilityBundle/Annotations/SessionControllerListener.php
+++ src/XN/UtilityBundle/Annotations/SessionControllerListener.php
@@ -43,7 +43,7 @@ class SessionControllerListener
         if($token)
             $token->getUser();
 
-        if(!$this->re->getMethodAnnotation(new \ReflectionMethod($controller[0], $controller[1]), 'XN\Annotations\UsesSession'))
+        if(!$this->re->getMethodAnnotation(new \ReflectionMethod($controller[0], $controller[1]), 'XN\Annotations\UsesSession') && !($controller instanceof \Sonata\AdminBundle\Controller\CRUDController))
             $this->se->save();
     }
 }
