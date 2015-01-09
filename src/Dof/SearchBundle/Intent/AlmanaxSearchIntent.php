ed7e28a7b248ad1c08dc2b5d108fdacee26ce993
Ener-Getick <energetick.guigui@gmail.com>
2015-01-09 23:19:43 +0100

    Add a text error

--- src/Dof/SearchBundle/Intent/AlmanaxSearchIntent.php
+++ src/Dof/SearchBundle/Intent/AlmanaxSearchIntent.php
@@ -11,6 +11,6 @@ class AlmanaxSearchIntent
         $quest = $this->em->getRepository('DofQuestBundle:Quest')
             ->findOneBy(['type' => QuestType::ALMANAX, 'season' => true]);
 
-        return !empty($quest) ? $this->tp->render('DofSearchBundle:Intent:almanax.html.twig', ['quest' => $quest]) : null;
+        return !empty($quest) ? $this->tp->render('DofSearchBundle:Intent:almanax.html.twig', ['quest' => $quest]) : "Quête almanax du jour non trouvée.";
     }
 }
