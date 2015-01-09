4cc15507073d9e0af39dd5a07fca98bc360e97aa
Ener-Getick <energetick.guigui@gmail.com>
2015-01-09 20:01:37 +0100

    noob error

--- src/Dof/SearchBundle/DofSearchBundle.php
+++ src/Dof/SearchBundle/DofSearchBundle.php
@@ -3,7 +3,16 @@
 namespace Dof\SearchBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Dof\SearchBundle\DependencyInjection\Compiler\SearchIntentCompilerPass;
 
 class DofSearchBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new SearchIntentCompilerPass());
+    }
 }
