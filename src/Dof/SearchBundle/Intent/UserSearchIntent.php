8224255765d7d0e3a503f57bea648374a20b947f
Ener-Getick <energetick.guigui@gmail.com>
2015-01-10 13:24:43 +0100

    [...]

--- src/Dof/SearchBundle/Intent/UserSearchIntent.php
+++ src/Dof/SearchBundle/Intent/UserSearchIntent.php
@@ -7,8 +7,8 @@ class UserSearchIntent
     private $perPage = 15;
 
     public function process(array $entities, $intent) : ?string {
-        $user = $this->em->getRepository('DofUserBundle:User')->findOneByUsername($entities['item']['username'])
+        $user = $this->em->getRepository('DofUserBundle:User')->findOneByUsername($entities['item']['username']);
 
-    return $this->tp->render('DofUserBundle:Profile:index.html.twig', ['user' => $user]);
-}
+        return $this->tp->render('DofUserBundle:Profile:index.html.twig', ['user' => $user]);
+    }
 }
