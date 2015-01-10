9c76c31637627e416160982e811c48cf6337bccc
Ener-Getick <energetick.guigui@gmail.com>
2015-01-10 13:47:53 +0100

    Add a render block function for the intents

--- src/Dof/SearchBundle/Intent/IntentTrait.php
+++ src/Dof/SearchBundle/Intent/IntentTrait.php
@@ -3,22 +3,31 @@ namespace Dof\SearchBundle\Intent;
 
 use XN\DependencyInjection\ServiceArray;
 
-use Symfony\Component\Templating\EngineInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 trait IntentTrait implements IntentInterface {
-    private EngineInterface $tp;
+    private \Twig_Environment $twig;
     private ObjectManager $em;
     private RequestStack $rs;
 
     public function __construct(ServiceArray $sa) {
-        $this->tp = $sa[0];
+        $this->twig = $sa[0];
         $this->em = $sa[1];
         $this->rs = $sa[2];
     }
 
     public function getLocale() {
         return $this->rs->getCurrentRequest()->getLocale();
+    }
+
+    public function render($template, $context)
+    {
+        return $this->twig->loadTemplate($template)->render($context);
+    }
+
+    public function renderBlock($template, $block, $context)
+    {
+        return $this->twig->loadTemplate($template)->renderBlock($block, $context);
     }
 }
--- src/Dof/SearchBundle/Intent/UserSearchIntent.php
+++ src/Dof/SearchBundle/Intent/UserSearchIntent.php
@@ -9,6 +9,6 @@ class UserSearchIntent
     public function process(array $entities, $intent) : ?string {
         $user = $this->em->getRepository('DofUserBundle:User')->findOneByUsername($entities['item']['username']);
 
-        return $this->tp->render('DofUserBundle:Profile:index.html.twig', ['user' => $user]);
+        return $this->render('DofUserBundle:Profile:index.html.twig', ['user' => $user]);
     }
 }
