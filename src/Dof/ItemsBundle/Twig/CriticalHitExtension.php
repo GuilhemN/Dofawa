1ffb6cccc95a2447cd3277eaa3eba46e9d30c65f
menthosaire <sebastien.michon17@gmail.com>
2015-01-17 15:50:19 +0100

    Fix bug

--- src/Dof/ItemsBundle/Twig/CriticalHitExtension.php
+++ src/Dof/ItemsBundle/Twig/CriticalHitExtension.php
@@ -12,7 +12,7 @@ class CriticalHitExtension extends \Twig_Extension
 
     public function getCriticalHit($denominator, $agility, $bonusCC)
     {
-        return (floor(($denominator - $bonusCC) * 2.9901 / Log($agility + 12));
+        return (floor(($denominator - $bonusCC) * 2.9901 / Log($agility + 12)));
     }
 
     public function getName()
