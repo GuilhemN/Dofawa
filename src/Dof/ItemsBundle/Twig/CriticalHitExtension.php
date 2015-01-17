3a3ba6fd0c8195d8f46f239a616a293146402bf1
menthosaire <sebastien.michon17@gmail.com>
2015-01-17 15:39:34 +0100

    Add CriticalHitExtension

---
+++ src/Dof/ItemsBundle/Twig/CriticalHitExtension.php
@@ -0,0 +1,22 @@
+<?php
+namespace Dof\ItemsBundle\Twig;
+
+class CriticalHitExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('criticalHit', array($this, 'CriticalHit')),
+        );
+    }
+
+    public function getCriticalHit($denominator, $agility)
+    {
+        return ($denominator * 2.9901 / Log($agility + 12));
+    }
+
+    public function getName()
+    {
+        return 'criticalHit_extension';
+    }
+}
