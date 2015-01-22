8cd5d0bb2f65b0bf495ee9df99b0809f35c603b8
Ener-Getick <energetick.guigui@gmail.com>
2015-01-22 20:39:10 +0100

    noob error

--- src/XN/UtilityBundle/Node/VariableNode.php
+++ src/XN/UtilityBundle/Node/VariableNode.php
@@ -3,12 +3,12 @@ namespace XN\UtilityBundle\Node;
 
 class VariableNode  extends \Twig_Node
 {
-    public function __construct($name, Twig_Node_Expression $value, $line, $tag = null)
+    public function __construct($name, \Twig_Node_Expression $value, $line, $tag = null)
     {
         parent::__construct(array('value' => $value), array('name' => $name), $line, $tag);
     }
 
-    public function compile(Twig_Compiler $compiler)
+    public function compile(\Twig_Compiler $compiler)
     {
         $compiler
         ->addDebugInfo($this)
