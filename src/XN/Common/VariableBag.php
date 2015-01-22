3dcd106c1a0bd5acd4be94c866fbdf7461338004
Ener-Getick <energetick.guigui@gmail.com>
2015-01-22 19:07:26 +0100

    another test

--- src/XN/Common/VariableBag.php
+++ src/XN/Common/VariableBag.php
@@ -78,13 +78,13 @@ class VariableBag implements \ArrayAccess
 	}
 
     public function offsetSet($offset, $value) {
-        return $this->set($offset, $value);
+        $this->set($offset, $value);
     }
     public function offsetExists($offset) {
         return $this->has($offset);
     }
     public function offsetUnset($offset) {
-        return $this->remove($offset);
+        $this->remove($offset);
     }
     public function offsetGet($offset) {
         return $this->get($offset);
