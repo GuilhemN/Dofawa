0c9cbda6d0f09faa5706a94b2b30b32a1726aaaa
Ener-Getick <energetick.guigui@gmail.com>
2015-01-28 15:09:51 +0100

    Add an intent to create a programmed notification

---
+++ src/Dof/SearchBundle/Intent/AddNotificationIntent.php
@@ -0,0 +1,34 @@
+<?hh
+namespace Dof\SearchBundle\Intent;
+
+use Dof\UserBundle\Entity\ProgrammedNotification;
+
+class AddNotificationIntent
+{
+    use IntentTrait;
+
+    public function process(array $entities, $intent) : ?string {
+        if(!$this->sc->isGranted('ROLE_USER'))
+            return $this->render('DofSearchBundle:Error:must-be-connected.html.twig');
+        $user = $this->sc->getToken()->getUser();
+        $type = $entities['type']['value'];
+        if($type == 'dragoturkey_childbirth') {
+            $drg = $this->em->getRepository('DofItemsBundle:Mount')->findOneByNameFr($entities['dragoturkey']['value']);
+            if($drg === null)
+                return 'Monture non trouvée.';
+            $pn = new ProgrammedNotification;
+            $pn
+                ->setOwner($user)
+                ->setType('dragoturkey_childbirth')
+                ->setEntity($drg)
+                ->setDate(new \Datetime('6 hours'));
+            $em
+                ->persist($pn)
+                ->flush($pn);
+            return $this->render('DofSearchBundle:Intent:dragoturkey-childbirth.html.twig', ['drg' => $drg]);
+        }
+
+
+        return '';
+    }
+}
