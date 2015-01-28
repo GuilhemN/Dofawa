5da0b37ff8241936d8090601c2997c1133e12b07
Ener-Getick <energetick.guigui@gmail.com>
2015-01-28 15:12:13 +0100

    [...]

--- src/Dof/SearchBundle/Intent/IntentTrait.php
+++ src/Dof/SearchBundle/Intent/IntentTrait.php
@@ -5,16 +5,19 @@ use XN\DependencyInjection\ServiceArray;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 trait IntentTrait implements IntentInterface {
     private \Twig_Environment $twig;
     private ObjectManager $em;
     private RequestStack $rs;
+    private SecurityContextInterface $sc;
 
     public function __construct(ServiceArray $sa) {
         $this->twig = $sa[0];
         $this->em = $sa[1];
         $this->rs = $sa[2];
+        $this->sc = $sa[3];
     }
 
     public function getLocale() {
