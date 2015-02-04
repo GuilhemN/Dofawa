522d6eedbc5f84c2394cb3afb39a7c0349c6cf07
Ener-Getick <energetick.guigui@gmail.com>
2015-02-04 11:25:23 +0100

    Fix a bug

--- src/Dof/Bundle/ItemBundle/DependencyInjection/DofItemExtension.php
+++ src/Dof/Bundle/ItemBundle/DependencyInjection/DofItemExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dof\Bundle\User\CharacterBundle\DependencyInjection;
+namespace Dof\Bundle\ItemBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DofBuildExtension extends Extension
+class DofItemExtension extends Extension
 {
     /**
      * {@inheritDoc}
--- src/Dof/Bundle/User/CharacterBundle/DependencyInjection/DofUserCharacterExtension.php
+++ src/Dof/Bundle/User/CharacterBundle/DependencyInjection/DofUserCharacterExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dof\Bundle\ItemBundle\DependencyInjection;
+namespace Dof\Bundle\User\CharacterBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DofItemsExtension extends Extension
+class DofUsrCharacterExtension extends Extension
 {
     /**
      * {@inheritDoc}
