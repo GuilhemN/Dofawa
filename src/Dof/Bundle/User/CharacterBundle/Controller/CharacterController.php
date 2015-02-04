649e8f4be0a8cd51282cf2eae23e9c53e42afb66
Ener-Getick <energetick.guigui@gmail.com>
2015-02-04 20:55:03 +0100

    Add a remove character action

--- src/Dof/Bundle/User/CharacterBundle/Controller/CharacterController.php
+++ src/Dof/Bundle/User/CharacterBundle/Controller/CharacterController.php
@@ -5,7 +5,9 @@ namespace Dof\Bundle\User\CharacterBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use XN\Annotations as Utils;
 
+use Symfony\Component\HttpFoundation\Request;
 use Dof\Bundle\UserBundle\Entity\User;
+use Dof\Bundle\User\CharacterBundle\Entity\PlayerCharacter;
 
 class CharacterController extends Controller
 {
@@ -20,5 +22,18 @@ class CharacterController extends Controller
         return $this->render('DofUserCharacterBundle:Character:index.html.twig', [
             'user' => $user
         ]);
+    }
+
+    /**
+     * @Utils\Secure("ROLE_USER")
+     */
+    public function removeAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $character = $em->getRepository('DofUserCharacterBundle:PlayerCharacter')->find($request->request->get('id'));
+        if(!$this->get('security.context')->isGranted('ROLE_ADMIN') && $character->getOwner() !== $this->getUser())
+            throw $this->createAccessDeniedException();
+        $em->remove($character);
+        $em->flush();
+        return $this->redirect($this->generateUrl('dof_user_character_homepage'));
     }
 }
