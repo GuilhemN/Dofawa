11e5029353308387721c30533fb658c87b32d8fc
Ener-Getick <energetick.guigui@gmail.com>
2015-02-04 20:35:18 +0100

    Change the controller that manages the build homepage

--- src/Dof/Bundle/User/CharacterBundle/Controller/CharacterController.php
+++ src/Dof/Bundle/User/CharacterBundle/Controller/CharacterController.php
@@ -5,15 +5,18 @@ namespace Dof\Bundle\User\CharacterBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use XN\Annotations as Utils;
 
+use Dof\Bundle\UserBundle\Entity\User;
+
 class CharacterController extends Controller
 {
     /**
      * @Utils\Secure("ROLE_USER")
      */
-    public function indexAction(){
-        $em = $this->getDoctrine()->getManager();
+    public function indexAction(User $user = null){
+        if(!$this->get('security.context')->isGranted('ROLE_ADMIN') && $user !== $this->getUser())
+            $em = $this->getDoctrine()->getManager();
         return $this->render('DofUserCharacterBundle:Character:index.html.twig', [
-            'user' => $this->getUser()
+            'user' => $user
         ]);
     }
 }
