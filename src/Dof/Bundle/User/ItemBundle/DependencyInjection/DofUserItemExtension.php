7ba272edb0a832606965cdf0cd1820b218dab2eb
Ener-Getick <energetick.guigui@gmail.com>
2015-02-04 11:31:02 +0100

    [...]

--- src/Dof/Bundle/User/ItemBundle/DependencyInjection/DofUserItemExtension.php
+++ src/Dof/Bundle/User/ItemBundle/DependencyInjection/DofUserItemExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DofItemsManagerExtension extends Extension
+class DofUserItemExtension extends Extension
 {
     /**
      * {@inheritdoc}
