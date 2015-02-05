cfaa498868ed8abd5e12f7ff4cc523da6166ae43
Ener-Getick <energetick.guigui@gmail.com>
2015-02-05 20:48:05 +0100

    Add a page to search monsters

--- src/Dof/Bundle/MonsterBundle/Controller/MonsterController.php
+++ src/Dof/Bundle/MonsterBundle/Controller/MonsterController.php
@@ -3,10 +3,34 @@
 namespace Dof\Bundle\MonsterBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 use Dof\Bundle\MonsterBundle\Entity\Monster;
 
 class MonsterController extends Controller
 {
+    const PER_PAGE = 20;
+
+    public function indexAction ($page, Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('DofMonsterBundle:Monster');
+        $options = $request->query->get('monster');
+        $locale = $this->get('translator')->getLocale();
+
+        $monsters = $repo->findWithOptions($options, ['level' => 'DESC'], self::PER_PAGE, ($page - 1) * self::PER_PAGE, $locale);
+        $count = $repo->countWithOptions($options, $locale);
+        $pagination = array(
+            'page' => $page,
+            'route' => $request->attributes->get('_route'),
+            'pages_count' => ceil($count / $perPage),
+            'route_params' => []
+        );
+        return $this->render('DofMonsterBundle:Monster:index.html.twig', [
+            'monsters' => $monsters,
+            'pagination' => $pagination
+            ]);
+    }
+
     public function showAction(Monster $monster)
     {
         $xRay = $this->get('security.context')->isGranted('ROLE_SPELL_XRAY');
--- src/Dof/Bundle/MonsterBundle/Entity/MonsterRepository.php
+++ src/Dof/Bundle/MonsterBundle/Entity/MonsterRepository.php
@@ -12,4 +12,43 @@ use Doctrine\ORM\EntityRepository;
  */
 class MonsterRepository extends EntityRepository
 {
+    public function findWithOptions($options = array(), array $orders = array(), $limit = null, $offset = null, $locale = 'fr', $type = 'normal', $full = false) {
+        $options = (array) $options;
+        $qb = $this->createQueryBuilder('m');
+
+        if($type == 'count')
+            $qb->select(array('COUNT(m)'));
+
+        if(!empty($options['name']))
+            $qb
+            ->andWhere('m.name' . ucfirst($locale).' LIKE :name')
+            ->setParameter('name', '%' . $options['name'] . '%')
+            ;
+        if(!empty($options['update']))
+            $qb
+                ->andWhere('m.release LIKE :release')
+                ->setParameter('release', '%' . $options['maj'] . '%')
+            ;
+        $qb->andWhere('m.deprecated = false');
+
+        foreach($orders as $column => $order)
+            $qb->addOrderBy('m.' . $column, $order);
+
+        if($type == 'count')
+            return $qb
+                ->getQuery()
+                ->getSingleScalarResult();
+
+        else
+            return $qb
+                ->getQuery()
+                ->setFirstResult($offset)
+                ->setMaxResults($limit)
+                ->getResult();
+                  ;
+    }
+    
+	public function countWithOptions($options = array(), $locale = 'fr'){
+		return $this->findWithOptions($options, array(), null, null, $locale, 'count');
+	}
 }
