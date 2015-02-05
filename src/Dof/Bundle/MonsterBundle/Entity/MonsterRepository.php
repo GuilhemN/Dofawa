1730a1643ce8e90c1c3a89819618e4725888feb5
Ener-Getick <energetick.guigui@gmail.com>
2015-02-05 21:20:54 +0100

    [...]

--- src/Dof/Bundle/MonsterBundle/Entity/MonsterRepository.php
+++ src/Dof/Bundle/MonsterBundle/Entity/MonsterRepository.php
@@ -32,7 +32,7 @@ class MonsterRepository extends EntityRepository
             ;
         $qb
             ->andWhere('m.deprecated = false')
-            ->addOrderBy('g.level', 'DESC')
+            ->addOrderBy('g.level', 'DESC');
 
         foreach($orders as $column => $order)
             $qb->addOrderBy('m.' . $column, $order);
@@ -44,7 +44,7 @@ class MonsterRepository extends EntityRepository
 
         else
             return $qb
-                ->groupBy('m.id');
+                ->groupBy('m.id')
                 ->getQuery()
                 ->setFirstResult($offset)
                 ->setMaxResults($limit)
