a7766c347f82a57d07494ff80a8af12e2391f033
Ener-Getick <energetick.guigui@gmail.com>
2015-02-07 18:32:42 +0100

    [...]

--- src/XN/Common/URLBuilder.php
+++ src/XN/Common/URLBuilder.php
@@ -103,7 +103,7 @@ class URLBuilder
         .((isset($parse_url['port'])) ? ':' . $parse_url['port'] : '')
         . '/'
         .((isset($parse_url['path'])) ? $parse_url['path'] : '')
-        .((isset($parse_url['query'])) ? '?' . $parse_url['query'] : '')
+        .((!empty($parse_url['query'])) ? '?' . $parse_url['query'] : '')
         .((isset($parse_url['fragment'])) ? '#' . $parse_url['fragment'] : '')
         ;
     }
