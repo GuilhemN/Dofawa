1edd968f5d3659aedb13fb0c57815192407fc77f
Ener-Getick <energetick.guigui@gmail.com>
2015-07-07 00:00:58 +0200

    remove the user slug

--- src/Dof/Bundle/UserBundle/Entity/User.php
+++ src/Dof/Bundle/UserBundle/Entity/User.php
@@ -33,7 +33,7 @@ class User extends BaseUser implements IdentifiableInterface, OwnableInterface, 
      */
     protected $id;
 
-    use TimestampableTrait, SluggableTrait, OwnableTrait, TOTPAuthenticatableTrait;
+    use TimestampableTrait,/* SluggableTrait,*/ OwnableTrait, TOTPAuthenticatableTrait;
 
     /**
      * @var \DateTime
@@ -55,13 +55,13 @@ class User extends BaseUser implements IdentifiableInterface, OwnableInterface, 
      */
     private $preferences;
 
-    /**
-     * @var string
-     *
-     * @Gedmo\Slug(fields={"username"})
-     * @ORM\Column(name="slug", type="string", nullable=false, unique=true)
-     */
-    protected $slug;
+    // /**
+    //  * @var string
+    //  *
+    //  * @Gedmo\Slug(fields={"username"})
+    //  * @ORM\Column(name="slug", type="string", nullable=false, unique=true)
+    //  */
+    // protected $slug;
 
     public function __construct()
     {
