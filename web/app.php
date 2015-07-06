9af56066d177cab1559a0aa37667d0857f5fc0e1
Ener-Getick <energetick.guigui@gmail.com>
2015-07-06 18:10:32 +0200

    Test to fix a bug

--- web/app.php
+++ web/app.php
@@ -4,7 +4,6 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 date_default_timezone_set('UTC');
-session_start();
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
